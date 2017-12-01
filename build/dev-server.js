dc63ab24207a3417cd3bc3dc0042338920ddaef9
S.H <23393331@qq.com>
2017-12-01 15:00:21 +0800

    edit config/index.js

--- build/dev-server.js
+++ build/dev-server.js
@@ -65,7 +65,7 @@ var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsS
 app.use(staticPath, express.static('./static'))
 
 var uri = 'http://localhost:' + port
-
+console.log(uri);
 var _resolve
 var readyPromise = new Promise(resolve => {
   _resolve = resolve
