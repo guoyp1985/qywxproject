e29067ed85fc9fb866750acc253e550e3eefaf16
guoyp1985 <605034716@qq.com>
2018-03-31 16:05:34 +0800

    add

---
+++ src/components/Listplate.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bk-listplate scroll_item">
+    <div class="piccell flex_left">
+      <slot name="pic"></slot>
+    </div>
+    <div class="titlecell flex_left">
+      <div style="width:100%;">
+        <slot name="title"></slot>
+      </div>
+    </div>
+    <div class="iconcell flex_right">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Listplate'
+}
+</script>
+
+<style lang="less">
+.bk-listplate{
+  display: -webkit-box;
+  padding:10px;
+  position:relative;
+  border-bottom:@list-border-color 1px solid;
+}
+.bk-listplate .piccell{
+  text-align:left;
+  width:50px;
+}
+.bk-listplate .titlecell{
+  flex:1;
+  text-align:left;
+}
+.bk-listplate .iconcell{
+  text-align:right;
+  width:60px;
+}
+.cols-2 .iconcell{display:none;}
+</style>
