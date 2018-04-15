599dccb2dec40127b89d95620ffc2680cff5b54e
simon <siemon83@gmail.com>
2018-04-15 11:37:45 +0800

    a

--- src/main.js
+++ src/main.js
@@ -177,6 +177,7 @@ router.afterEach(function (to) {
 
 // Vue.http.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbGFyYXZlbC5ib2thLmNuL2FwaS9zY2FubG9naW4vMTUyMzUwNDEwOSIsImlhdCI6MTUyMzUwNDE0NywiZXhwIjoxNTI0MzY4MTQ3LCJuYmYiOjE1MjM1MDQxNDcsImp0aSI6IlFrRFRwOEd2WGlsd1lqR3kiLCJzdWIiOjEsInBydiI6Ijg2NjVhZTk3NzVjZjI2ZjZiOGU0OTZmODZmYTUzNmQ2OGRkNzE4MTgifQ.bRfinjIiBjiFXXCZru1Nhw_0l8RD7Zf7FWOhv1Aw4W8'
 let once = true
+let switcher = true
 Vue.http.interceptors.push(function (request, next) {
   const rUrl = urlParse(request.url)
   const lUrl = urlParse(location.href, true)
@@ -191,13 +192,14 @@ Vue.http.interceptors.push(function (request, next) {
     .then(data => {
       const openId = data.data
       OpenId.set(openId)
+      switcher = false
       Vue.http.get(`${ENV.BokaApi}/api/login/${openId}`, {})
       .then(res => res.json())
       .then(data => {
         alert(JSON.stringify(data))
       })
     })
-  } else if (rUrl.origin === ENV.BokaCDN) {
+  } else if (rUrl.origin === ENV.BokaCDN && switcher) {
     // next(function (response) {
     // Vue.http.get(`${ENV.WxOAuthUrl}appid=${ENV.AppId}&secret=${ENV.AppSecret}&code=${code}&grant_type=authorization_code`, {})
     // .then(res => {
