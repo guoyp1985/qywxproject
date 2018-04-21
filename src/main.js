373171d8f6f74faa2d5fea7fdd96e95906613a34
simon <siemon83@gmail.com>
2018-04-21 13:45:07 +0800

    update by simon

--- src/main.js
+++ src/main.js
@@ -149,7 +149,7 @@ let excludeUrls = [
   { url: `${ENV.BokaApi}/api/qrcode/login*`, reqMax: 1 },
   { url: `${ENV.BokaApi}/api/login/*`, reqMax: 1 },
   { url: `${ENV.BokaApi}/api/scanlogin`, reqMax: 1 },
-  { url: `${ENV.BokaApi}/api/user/address/list`, reqMax: 2 }
+  { url: `${ENV.BokaApi}/api/user/address/list`, reqMax: 4 }
 ]
 
 // 排除全局请求过滤器中的请求url
