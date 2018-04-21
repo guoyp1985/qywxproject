0e957403bce9ec7f92c8660cb2252de3d3b091ac
simon <siemon83@gmail.com>
2018-04-21 11:52:18 +0800

    update by simon

--- src/pages/Address.vue
+++ src/pages/Address.vue
@@ -98,7 +98,7 @@ export default {
         const accessToken = data.access_token
         const nonceStr = this.$util.randomStr()
         const timeStamp = this.$uitl.timeStamp()
-        const url = location.href.replace(/#\//g, '')
+        const url = location.href.replace(/\#\//g, '')
         alert(url)
         const addrSign = this.$util.wxSign(accessToken, ENV.AppId, nonceStr, timeStamp, url)
         WeixinJSBridge.invoke('editAddress', {
