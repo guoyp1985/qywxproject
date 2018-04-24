ce65c59b8170eb0ea1c893d303f3a4c625e4f1e4
simon <siemon83@gmail.com>
2018-04-24 20:48:22 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -182,7 +182,7 @@ Vue.http.interceptors.push(function (request, next) {
       data => {
         Token.set(data.data.token)
         // token = data.data.token // test
-        location.href = `http://${lUrl.hostname}/${lUrl.hash}`
+        // location.href = `http://${lUrl.hostname}/${lUrl.hash}`
         // alert(data.data.weixin_token)
         // const accessToken = data.data.weixin_token
         // const nonceStr = $vue.$util.randomStr(6)
