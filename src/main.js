f43da59309d18e55b190e9f83f8acefa07728aff
simon <siemon83@gmail.com>
2018-04-24 19:49:19 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -165,7 +165,7 @@ const matchExclude = url => {
   return false
 }
 
-let token = null // test
+// let token = null // test
 // 全局请求过滤器
 Vue.http.interceptors.push(function (request, next) {
   const rUrl = urlParse(request.url)
