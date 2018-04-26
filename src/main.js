f9f8c8fc0254e01540e9d51a68ab988dc7124c50
simon <siemon83@gmail.com>
2018-04-26 10:55:44 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -242,7 +242,7 @@ function (error) {
 Vue.http.interceptors.response.use(function (response) {
   return response
 }, function (error) {
-  alert(error.request.baseURL)
+  alert(error.request)
   $vue.$util.access(error.response, isPC => {
     if (isPC) {
       // Vue.http.get(`${ENV.BokaApi}/api/qrcode/login`)
