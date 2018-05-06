af6dc99fa52738773dbec9b463d57654634cc464
S.H <23393331@qq.com>
2018-05-06 12:45:02 +0800

    upload by simon

--- libs/env.js
+++ libs/env.js
@@ -1,5 +1,5 @@
 const ENV = {
-  AppId: 'wx1e72f0c45242a94e', //'wxd50f3623bec2f5b5', //'wxd50f3623bec2f5b5', //'wx1e72f0c45242a94e'
+  AppId: 'wxd50f3623bec2f5b5', //'wx1e72f0c45242a94e'
   // AppSecret: 'f22214afc8a01ec9a769bd0ba8551c48',
   FeedChars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
   WxAuthUrl: 'https://open.weixin.qq.com/connect/oauth2/authorize?',
