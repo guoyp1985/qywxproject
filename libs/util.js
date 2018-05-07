60aa17900af03eaca1ad0f5f01806276031aacc7
S.H <23393331@qq.com>
2018-05-07 16:31:25 +0800

    upload by simon

--- libs/util.js
+++ libs/util.js
@@ -16,7 +16,7 @@ Util.install = function (Vue, options) {
     // 判终端
     isPC: function () {
       const userAgentInfo = navigator.userAgent
-      alert(userAgentInfo)
+      alert(Reg.rPlatfrom.test(userAgentInfo))
       if (Reg.rPlatfrom.test(userAgentInfo)) {
         return false
       }
