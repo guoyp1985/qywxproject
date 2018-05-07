abdbc2c93f659162a2fb5eb8883826961c5c7054
simon <siemon83@gmail.com>
2018-05-08 05:15:20 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -275,9 +275,9 @@ let removePending = (config) => {
 // Token.remove()
 // 请求拦截器
 Vue.http.interceptors.request.use(config => {
-  removePending(config)
+  // removePending(config)
   config.cancelToken = new CancelToken(c => {
-    pending.push({ u: config.url + '&' + config.method, f: c })
+    // pending.push({ u: config.url + '&' + config.method, f: c })
   })
   const token = Token.get()
   alert(token)
@@ -294,7 +294,7 @@ Vue.http.interceptors.request.use(config => {
 
 // 响应拦截器
 Vue.http.interceptors.response.use(response => {
-  removePending(response.config)
+  // removePending(response.config)
   return response
 }, error => {
   // alert(JSON.stringify(error))
