98c30e7af927854d13528fe1a4be1010662e4f5b
S.H <23393331@qq.com>
2018-05-07 15:55:22 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -275,9 +275,9 @@ let removePending = (config) => {
 // 请求拦截器
 Vue.http.interceptors.request.use(config => {
   // removePending(config)
-  config.cancelToken = new CancelToken(c => {
-    pending.push({ u: config.url + '&' + config.method, f: c })
-  })
+  // config.cancelToken = new CancelToken(c => {
+  //   pending.push({ u: config.url + '&' + config.method, f: c })
+  // })
   const token = Token.get()
   // alert(token)
   config.headers['Authorization'] = `Bearer ${token}`
