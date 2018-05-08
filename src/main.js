0d0128b9d7ef892617d1f81897cde718334fd01a
simon <siemon83@gmail.com>
2018-05-08 13:22:09 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -316,7 +316,7 @@ Vue.http.interceptors.response.use(function (response) {
     .then(
       res => {
         Token.set(res.data.data.token)
-        alert('token')
+        // alert('token')
         // getAddress(res.data.data.weixin_token)
         return Vue.http.get(`${ENV.BokaApi}/api/user/show`)
       }
