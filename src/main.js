2196bb462c29d2aad746e968d6ddddefba45cf59
simon <siemon83@gmail.com>
2018-05-08 12:11:57 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -19,10 +19,10 @@ import WeixinJSBridge from 'WeixinJSBridge'
 const CancelToken = AjaxPlugin.$http.CancelToken
 Vue.use(AjaxPlugin)
 Vue.use(Vuex)
-console.log(AjaxPlugin.defaults)
-AjaxPlugin.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-AjaxPlugin.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
-AjaxPlugin.defaults.withCredentials = true;
+console.log(AjaxPlugin.$http.defaults)
+AjaxPlugin.$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+AjaxPlugin.$http.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+AjaxPlugin.$http.defaults.withCredentials = true;
 require('es6-promise').polyfill()
 let store = new Vuex.Store({
   modules: {
