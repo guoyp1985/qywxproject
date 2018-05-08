7fb384c873f183418e1675dee0bfd01a22e7ad27
simon <siemon83@gmail.com>
2018-05-08 13:26:22 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -307,7 +307,7 @@ Vue.http.interceptors.response.use(function (response) {
   // alert(JSON.stringify(error))
   const lUrl = urlParse(location.href, true)
   const code = lUrl.query.code
-  const access = Access.get()
+  // const access = Access.get()
   // alert($vue.$util.isAndroid()+','+!access+','+code)
   // if ($vue.$util.isAndroid() && !access && code) {
   //   Access.set(true)
@@ -330,7 +330,7 @@ Vue.http.interceptors.response.use(function (response) {
   //   )
   // } else
   if (code) {
-    Access.set(true)
+    // Access.set(true)
     Vue.http.get(`${ENV.BokaApi}/api/authLogin/${code}`)
     .then(
       res => {
