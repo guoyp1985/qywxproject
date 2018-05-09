fcfbe135781c6e24f0c7617cbcfdab38732e5002
guoyp1985 <605034716@qq.com>
2018-05-09 16:57:24 +0800

    gypadd

--- src/pages/Bargainbuy.vue
+++ src/pages/Bargainbuy.vue
@@ -157,6 +157,9 @@ export default {
         let data = res.data
         self.cutData = data.data ? data.data : data
       })
+    },
+    closeShareSuccess () {
+      this.showShareSuccess = false
     }
   },
   created () {
