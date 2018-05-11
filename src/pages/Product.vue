307a9e8d2e440955ad35f81773c81f1b11e39469
guoyp1985 <605034716@qq.com>
2018-05-11 11:54:48 +0800

    gypadd

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -537,6 +537,7 @@ export default {
     self.query = self.$route.query
     self.productid = self.query.id
     self.loginUser = User.get()
+    /*
     if (self.loginUser) {
       self.isshowtop = true
       setTimeout(function () {
@@ -548,6 +549,7 @@ export default {
         self.showsharetip = false
       }, 10000)
     }
+    */
     let infoparams = { id: self.productid, module: 'product' }
     if (self.query.wid) {
       infoparams['wid'] = self.query.wid
@@ -582,6 +584,7 @@ export default {
         self.contentphotoarr = self.productdata.contentphoto.split(',')
         self.previewerPhotoarr = self.$util.previewerImgdata(self.contentphotoarr)
       }
+      /*
       let shareData = {
         module: 'product',
         moduleid: self.productdata.id,
@@ -610,7 +613,10 @@ export default {
       return self.$http.get(`${ENV.BokaApi}/api/retailer/friendBuy`, {
         params: buyparams
       })
-    }).then(function (res) {
+      */
+    })
+    /*
+    .then(function (res) {
       let data = res.data
       if (data.flag === 1) {
         self.buyuserdata = (data.data ? data.data : data)
@@ -644,6 +650,7 @@ export default {
         self.activitydata = data.data ? data.data : data
       }
     })
+    */
   }
 }
 </script>
