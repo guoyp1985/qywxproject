78c3f8a52e7fdde422fe245ae2be453c70629395
guoyp1985 <605034716@qq.com>
2018-05-15 10:08:25 +0800

    gypadd

--- libs/util.js
+++ libs/util.js
@@ -404,6 +404,7 @@ Util.install = function (Vue, options) {
                   localId: d.toString(),
                   isShowProgressTips: 0,
                   success: function (res1) {
+                    alert(JSON.stringify(res1))
                     self.$http.post(`${ENV.BokaApi}/api/weixinUpload`, {
                       imgid: res1.serverId
                     }).then(function (res) {
