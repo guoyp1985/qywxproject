cb5a6f38c56e945f6ec8607ca2aa63b07667ab14
guoyp1985 <605034716@qq.com>
2018-05-21 15:51:40 +0800

    gypadd

--- src/pages/Messages.vue
+++ src/pages/Messages.vue
@@ -1,54 +1,81 @@
-/*
-* @description: 消息页
-* @auther: simon
-* @created_date: 2018-4-20
-*/
 <template>
-  <div id="message-list">
-    <group>
-      <group-title slot="title">{{$t('Message')}}</group-title>
-      <cell v-for="(item, index) in list" :key="index" :title="item.userName" :value="item.date | dateFormat">
-        <img class="user-avatar" slot="icon" style="display:block;margin-right:5px;" src="">
-        <span slot="inline-desc">
-          {{item.message}}
-        </span>
-      </cell>
-    </group>
+  <div class="containerarea bg-white font14">
+    <div class="s-topbanner" style="height:44px;">
+      <div class="row">
+        <div class="bg"></div>
+        <div class="flex_center h_100 toprow">
+          <div class="flex_cell font18 pl20">{{$t('Message')}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="s-container scroll-container" style="top:44px;">
+      <div class="scroll_list pl10 pr10">
+        <router-link :to="{path: '/chat', query: {uid: item.uid}}" class="scroll_item pt10 pb10 db" v-for="(item,index) in data" :key="item.id">
+          <div class="t-table">
+            <div class="t-cell v_middle align_left" style="width:50px;">
+              <x-img class="avatarimg1 imgcover" :src="item.avatar" default-src="../src/assets/images/user.jpg" :offset="0" container=".scroll-container"></x-img>
+            </div>
+            <div class="t-cell v_middle align_left">
+              <div class="clamp1"><span :class="getDateClass(item.dateline)">{{ getDateState(item.dateline) }}</span><span>{{ item.linkman }}</span></div>
+              <div class="clamp1 font12 color-gray">{{ item.title }}</div>
+              <div class="clamp1 color-gray font12">{{ item.dateline | dateformat }}</div>
+            </div>
+            <div v-if="item.unreadNumber > 0" class="t-cell v_middle align_right w60">
+              <div class="qbtn6 bg-green color-white">{{ item.unreadNumber }}</div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
+
+<i18n>
+Message:
+  zh-CN: 消息
+</i18n>
+
 <script>
-import { GroupTitle, Group, Cell } from 'vux'
+import { XImg } from 'vux'
 import Time from '#/time'
+import ENV from 'env'
+
 export default {
   components: {
-    GroupTitle, Group, Cell
+    XImg
+  },
+  filters: {
+    dateformat: function (value) {
+      return new Time(value * 1000).dateFormat('yyyy-MM-dd hh:mm')
+    }
   },
   data () {
     return {
-      list: [
-        {
-          avatar: '',
-          userName: 'simon',
-          message: '个人中心',
-          date: 1522659301220
-        }
-      ]
+      data: []
     }
   },
-  filters: {
-    dateFormat (value) {
-      return new Time(value).dateFormat('yyyy-MM-dd hh:mm')
+  methods: {
+    getDateState: function (dt) {
+      return this.$util.getDateState(dt)
+    },
+    getDateClass: function (dt) {
+      let ret = this.$util.getDateClass(dt)
+      ret = `${ret} mr5`
+      return ret
     }
   },
   created () {
-
+    const self = this
+    self.$store.commit('updateToggleTabbar', {toggleBar: false})
+    self.$vux.loading.show()
+    self.$http.get(`${ENV.BokaApi}/api/message/mylist`).then(function (res) {
+      let data = res.data
+      self.$vux.loading.hide()
+      self.data = data.data ? data.data : data
+    })
   }
 }
 </script>
+
 <style lang="less" scoped>
-#message-list .user-avatar {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-}
 </style>
