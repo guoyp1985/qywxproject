a9dfb53320997997b733817d4908ff70eecce6d0
cxj <caoxuejia@sina.com>
2018-05-23 09:50:54 +0800

    'addcxj'

--- src/components/Orderitemplate.vue
+++ src/components/Orderitemplate.vue
@@ -1,13 +1,13 @@
 <template>
-  <router-link :to="{path: '/retailerOrderDetail', query: {id: data.id}}" class="bk-orderitem order-item scroll_item mb10 font14 db">
-    <div class="bg-white padding12 b_bottom_after font13 flex_center list-shadow">
+  <router-link :to="{path: '/retailerOrderDetail', query: {id: data.id}}" class="bk-orderitem order-item scroll_item mb10 font14 db list-shadow">
+    <div class="bg-white padding12 b_bottom_after font13 flex_center ">
         <div class="flex_cell color-lightgray"><!-- 创建时间：<slot name="createdate"></slot> -->
           {{ $t('Order Number')}}：<slot name="orderno"></slot>
         </div>
         <div class="align_right color-red4" style="width:100px;"><slot name="flagstr"></slot></div>
     </div>
     <slot name="productlist"></slot>
-    <div class="bg-white padding10 b_top_after b_bottom_after color-gray5 list-shadow">
+    <div class="bg-white padding10 b_top_after b_bottom_after color-gray5">
         <slot name="receivearea"></slot>
     </div>
   </router-link>
