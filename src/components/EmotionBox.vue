4b5bd7557e2a8e03cb2032e95d8997feb477f5a9
guoyp1985 <605034716@qq.com>
2018-05-23 10:47:19 +0800

    gypadd

--- src/components/EmotionBox.vue
+++ src/components/EmotionBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="emotion-box">
-    <swiper :show-desc-mask="false" dots-position="center" height="160px">
+    <swiper :show-desc-mask="false" dots-position="center" height="200px">
       <swiper-item class="swiper-page">
         <emotion v-for="(item, index) in emotions1" :key="index" is-gif @click.native.stop="onClick(item)">{{item}}</emotion>
         <div class="emotion-delete" @click="onDelete">
