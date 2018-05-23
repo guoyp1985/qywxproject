dbe57b6d19124aca995f7ce43e8a5dd1d8b6be3c
S.H <23393331@qq.com>
2018-05-23 18:06:19 +0800

    upload by simon

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -263,7 +263,7 @@ export default {
       const user = User.get()
       const lUrl = urlParse(location.href, true)
       const code = lUrl.query.code
-      alert(user.subscribe)
+      // alert(user.subscribe)
       if (user && user.subscribe === 0) {
         if (code) {
           this.$http.get(`${ENV.BokaApi}/api/authUser/${code}`)
