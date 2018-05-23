bb5e0e169bb84d462dc7dfaf63aa2148b7500f6e
S.H <23393331@qq.com>
2018-05-23 17:07:58 +0800

    upload by simon

--- src/components/CTitle.vue
+++ src/components/CTitle.vue
@@ -12,7 +12,7 @@
           <div class="user-info">
             <router-link :to="{ name: 'tProfile', params: { profile: profile } }">
               <div>
-                <x-img :src="avatar" default-src="../src/assets/images/user.jpg"/>
+                <x-img :src="avatar" default-src="../src/assets/images/user.jpg"></x-img>
               </div>
               <div class="user-name">
                 <span class="color-white font16 u-name">{{ name }}</span>
