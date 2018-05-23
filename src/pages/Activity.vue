cdcd54d5fab9c5bc5c40353fb352d77ad2c66ec1
S.H <23393331@qq.com>
2018-05-23 17:46:45 +0800

    upload by simon

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -282,6 +282,9 @@ export default {
         }
       } else {
         this.$http.get(`${ENV.BokaApi}/api/user/show`)
+        .then(res => {
+          alert(JSON.stringify(res.data))
+        })
       }
     }
   },
