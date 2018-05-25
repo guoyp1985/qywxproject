3d193d7d6f370f281a3d5a1759fca46faa026425
guoyp1985 <605034716@qq.com>
2018-05-25 10:52:07 +0800

    gypadd

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,129 +1,134 @@
 <template>
   <div class="containerarea bg-page font14 s-havebottom store">
-    <div class="s-container scroll-container" style="top:0px;" ref="scrollContainer" @scroll="handleScroll">
-      <swiper
-        class="pic-swiper notitle"
-        v-if="addata && addata.length > 0"
-        :list="addata"
-        dots-position="center"
-        :interval=6000
-        :show-dots="isshowdot"
-        :aspect-ratio="500/900"
-        auto
-        loop>
-      </swiper>
-      <template v-if="retailerInfo.uid">
-        <div class="pt12 pb12 bg-white pl10 pr10 b_bottom_after">
-      		<div class="t-table">
-      			<div class="t-cell v_middle w50">
-              <x-img class="avatarimg1 imgcover" :src="retailerInfo.avatar" default-src="../src/assets/images/user.jpg" :offset="0" container=".scroll-container"></x-img>
+    <template v-if="showSos">
+      <Sos :title="sosTitle"></Sos>
+    </template>
+    <template v-if="showContainer">
+      <div class="s-container scroll-container" style="top:0px;" ref="scrollContainer" @scroll="handleScroll">
+        <swiper
+          class="pic-swiper notitle"
+          v-if="addata && addata.length > 0"
+          :list="addata"
+          dots-position="center"
+          :interval=6000
+          :show-dots="isshowdot"
+          :aspect-ratio="500/900"
+          auto
+          loop>
+        </swiper>
+        <template v-if="retailerInfo.uid">
+          <div class="pt12 pb12 bg-white pl10 pr10 b_bottom_after">
+        		<div class="t-table">
+        			<div class="t-cell v_middle w50">
+                <x-img class="avatarimg1 imgcover" :src="retailerInfo.avatar" default-src="../src/assets/images/user.jpg" :offset="0" container=".scroll-container"></x-img>
+        			</div>
+        			<div class="t-cell v_middle shopkeeper_txt">
+        				<div class="clamp1 font16">{{ retailerInfo.title }}</div>
+        			</div>
+        				<div v-if="retailerInfo.uid == loginUser.uid" class="t-cell v_middle align_right" style="width:160px;">
+                  <router-link class="font12 color-gray5 mr5 v_middle" to="/decorationShop"><i class="al al-dianpu font18 color-red"></i>{{$t('Rolling display')}}</router-link>
+                  <router-link class="font12 color-gray5 v_middle" to="/centerSales"><i class="al al-xiaoshou font18 color-red"></i>{{$t('Manage center')}}</router-link>
+        				</div>
+                <div v-else class="t-cell v_middle align_right w100">
+                  <div :class="`collect btnfavorite ${favoritecss}`" @click="favoriteevent"><i class="al al-xing font13 v_middle staricon"></i><span class="txt v_middle"></span></div>
+                </div>
       			</div>
-      			<div class="t-cell v_middle shopkeeper_txt">
-      				<div class="clamp1 font16">{{ retailerInfo.title }}</div>
-      			</div>
-      				<div v-if="retailerInfo.uid == loginUser.uid" class="t-cell v_middle align_right" style="width:160px;">
-                <router-link class="font12 color-gray5 mr5 v_middle" to="/decorationShop"><i class="al al-dianpu font18 color-red"></i>{{$t('Rolling display')}}</router-link>
-                <router-link class="font12 color-gray5 v_middle" to="/centerSales"><i class="al al-xiaoshou font18 color-red"></i>{{$t('Manage center')}}</router-link>
-      				</div>
-              <div v-else class="t-cell v_middle align_right w100">
-                <div :class="`collect btnfavorite ${favoritecss}`" @click="favoriteevent"><i class="al al-xing font13 v_middle staricon"></i><span class="txt v_middle"></span></div>
-              </div>
-    			</div>
-    		</div>
-      </template>
-      <template v-if="activitydata && activitydata.length > 0">
-        <div class="bg-white mt5 padding10 b_top_after">
-    			<span class="db-in pl5 font16 vline">{{ $t('Selection promotion') }}</span>
-    		</div>
-        <div class="b_top_after"></div>
-        <div class="activitylist">
-          <div v-for="(item,index) in activitydata" :key="item.id" class="bg-page">
-            <groupbuyitemplate :data="item" v-if="item.type == 'groupbuy'">
-              <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width:80px;height:80px;" :offset="0" container=".scroll-container"></x-img>
-              <span slot="title">{{ item.title }}</span>
-              <span slot="numbers">{{ item.numbers }}</span>
-              <span slot="havetuan">{{ item.havetuan }}</span>
-              <span slot="groupprice">{{ item.groupprice }}</span>
-              <span slot="price">{{ item.price }}</span>
-            </groupbuyitemplate>
-            <bargainbuyitemplate :data="item" v-if="item.type == 'bargainbuy'">
-              <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width:80px;height:80px;" :offset="0" container=".scroll-container"></x-img>
-              <span slot="title">{{ item.title }}</span>
-              <span slot="saveprice">{{ item.saveprice }}</span>
-              <span slot="minprice">{{ item.minprice }}</span>
-              <span slot="price">{{ item.price }}</span>
-            </bargainbuyitemplate>
-          </div>
-        </div>
-      </template>
-      <div class="bg-white mt5 padding10 b_top_after">
-  			<span class="db-in pl5 font16 vline">{{ $t('All products') }}</span>
-  		</div>
-      <div class="b_top_after"></div>
-      <div v-if="disproductdata" class="productlist squarepic">
-        <div v-if="productdata.length == 0" class="emptyitem flex_center">暂无商品</div>
-        <productitemplate v-else :data="item" v-for="(item,index) in productdata" :key="item.id">
-          <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" :offset="0" container=".scroll-container"></x-img>
-          <span slot="title">{{ item.title }}</span>
-          <span slot="price" style="margin-left:1px;">{{ item.price }}</span>
-          <span slot="saled" style="margin-left:1px;">{{ item.saled }}</span>
-        </productitemplate>
-      </div>
-      <template v-if="toplinedata.length > 0">
-        <div class="bg-white mt5 padding10 b_top_after">
-          <div class="t-table">
-            <div class="t-cell v_middle align_left">
-      			     <span class="db-in pl5 font16 vline">{{ $t('Shop topline') }}</span>
-            </div>
-            <div class="t-cell v_middle align_right">
-              <div class="qbtn4" style="padding: 3px 8px;line-height: 1;" @click="changeNews">
-    						<i class="al al-shuaxin4 font12 mr3"></i><span>{{ $t('Another batch') }}</span>
-    					</div>
+      		</div>
+        </template>
+        <template v-if="activitydata && activitydata.length > 0">
+          <div class="bg-white mt5 padding10 b_top_after">
+      			<span class="db-in pl5 font16 vline">{{ $t('Selection promotion') }}</span>
+      		</div>
+          <div class="b_top_after"></div>
+          <div class="activitylist">
+            <div v-for="(item,index) in activitydata" :key="item.id" class="bg-page">
+              <groupbuyitemplate :data="item" v-if="item.type == 'groupbuy'">
+                <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width:80px;height:80px;" :offset="0" container=".scroll-container"></x-img>
+                <span slot="title">{{ item.title }}</span>
+                <span slot="numbers">{{ item.numbers }}</span>
+                <span slot="havetuan">{{ item.havetuan }}</span>
+                <span slot="groupprice">{{ item.groupprice }}</span>
+                <span slot="price">{{ item.price }}</span>
+              </groupbuyitemplate>
+              <bargainbuyitemplate :data="item" v-if="item.type == 'bargainbuy'">
+                <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width:80px;height:80px;" :offset="0" container=".scroll-container"></x-img>
+                <span slot="title">{{ item.title }}</span>
+                <span slot="saveprice">{{ item.saveprice }}</span>
+                <span slot="minprice">{{ item.minprice }}</span>
+                <span slot="price">{{ item.price }}</span>
+              </bargainbuyitemplate>
             </div>
           </div>
+        </template>
+        <div class="bg-white mt5 padding10 b_top_after">
+    			<span class="db-in pl5 font16 vline">{{ $t('All products') }}</span>
     		</div>
         <div class="b_top_after"></div>
-        <div class="productlist">
-          <newsitemplate :data="item" v-for="(item,index) in toplinedata" :key="item.id">
-            <x-img slot="photo" class="v_middle imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width: 70px; height: 50px;" :offset="0" container=".scroll-container"></x-img>
+        <div v-if="disproductdata" class="productlist squarepic">
+          <div v-if="productdata.length == 0" class="emptyitem flex_center">暂无商品</div>
+          <productitemplate v-else :data="item" v-for="(item,index) in productdata" :key="item.id">
+            <x-img slot="photo" class="imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" :offset="0" container=".scroll-container"></x-img>
             <span slot="title">{{ item.title }}</span>
-            <span slot="date">{{ item.dateline | dateformat }}</span>
-          </newsitemplate>
+            <span slot="price" style="margin-left:1px;">{{ item.price }}</span>
+            <span slot="saled" style="margin-left:1px;">{{ item.saled }}</span>
+          </productitemplate>
         </div>
-      </template>
-      <div class="padding10">
-        <div class="btn-open" @click="openShop" style="display: block;background-color: #e10c00">我也要开店</div>
+        <template v-if="toplinedata.length > 0">
+          <div class="bg-white mt5 padding10 b_top_after">
+            <div class="t-table">
+              <div class="t-cell v_middle align_left">
+        			     <span class="db-in pl5 font16 vline">{{ $t('Shop topline') }}</span>
+              </div>
+              <div class="t-cell v_middle align_right">
+                <div class="qbtn4" style="padding: 3px 8px;line-height: 1;" @click="changeNews">
+      						<i class="al al-shuaxin4 font12 mr3"></i><span>{{ $t('Another batch') }}</span>
+      					</div>
+              </div>
+            </div>
+      		</div>
+          <div class="b_top_after"></div>
+          <div class="productlist">
+            <newsitemplate :data="item" v-for="(item,index) in toplinedata" :key="item.id">
+              <x-img slot="photo" class="v_middle imgcover" :src="item.photo" default-src="../src/assets/images/nopic.jpg" style="width: 70px; height: 50px;" :offset="0" container=".scroll-container"></x-img>
+              <span slot="title">{{ item.title }}</span>
+              <span slot="date">{{ item.dateline | dateformat }}</span>
+            </newsitemplate>
+          </div>
+        </template>
+        <div class="padding10">
+          <div class="btn-open" @click="openShop" style="display: block;background-color: #e10c00">我也要开店</div>
+        </div>
       </div>
-    </div>
-    <div class="s-bottom flex_center">
-      <router-link :to="{path: '/chat', query: {uid: query.wid}}" class="flex_cell bg-orange1 color-white h_100 flex_center" style="border-right:#fff 1px solid;"><i class="al al-pinglun color-fff font18" style="padding-right:3px;"></i>{{ $t('Online consulting') }}</router-link>
-      <div class="flex_cell bg-red color-white h_100 flex_center" @click="clickWetchat"><i class="al al-weixin2 color-fff font18" style="padding-right:3px;"></i>{{ $t('Wechat contact') }}</div>
-    </div>
-    <div v-transfer-dom class="x-popup">
-      <popup v-model="showqrcode" height="100%">
-        <div class="popup1 h_100 flex_center font14 bg-black">
-          <div class="flex_center" style="width:82%;height:82%;">
-            <div class="align_center">
-              <img :src="retailerInfo.qrcode" class="db" style="max-width:100%;max-height:100%;margin:0 auto 15px;">
-      				<div class="color-white bg-blue padding5 border-box" style="background-color: #0f96e8;border-radius:4px;">长按识别二维码成为微信好友</div>
-      				<div style="margin-top:30px;">
-      					<span class="db-in" @click="closepopup">
-      						<i class="al al-close color-white font36"></i>
-      					</span>
-      				</div>
+      <div class="s-bottom flex_center">
+        <router-link :to="{path: '/chat', query: {uid: query.wid}}" class="flex_cell bg-orange1 color-white h_100 flex_center" style="border-right:#fff 1px solid;"><i class="al al-pinglun color-fff font18" style="padding-right:3px;"></i>{{ $t('Online consulting') }}</router-link>
+        <div class="flex_cell bg-red color-white h_100 flex_center" @click="clickWetchat"><i class="al al-weixin2 color-fff font18" style="padding-right:3px;"></i>{{ $t('Wechat contact') }}</div>
+      </div>
+      <div v-transfer-dom class="x-popup">
+        <popup v-model="showqrcode" height="100%">
+          <div class="popup1 h_100 flex_center font14 bg-black">
+            <div class="flex_center" style="width:82%;height:82%;">
+              <div class="align_center">
+                <img :src="retailerInfo.qrcode" class="db" style="max-width:100%;max-height:100%;margin:0 auto 15px;">
+        				<div class="color-white bg-blue padding5 border-box" style="background-color: #0f96e8;border-radius:4px;">长按识别二维码成为微信好友</div>
+        				<div style="margin-top:30px;">
+        					<span class="db-in" @click="closepopup">
+        						<i class="al al-close color-white font36"></i>
+        					</span>
+        				</div>
+              </div>
             </div>
           </div>
-        </div>
-      </popup>
-    </div>
-    <share-success
-      v-show="showShareSuccess"
-      v-if="retailerInfo.uploader == loginUser.uid || retailerInfo.identity != 'user'"
-      :data="retailerInfo"
-      :loginUser="loginUser"
-      module="store"
-      :on-close="closeShareSuccess">
-    </share-success>
+        </popup>
+      </div>
+      <share-success
+        v-show="showShareSuccess"
+        v-if="retailerInfo.uploader == loginUser.uid || retailerInfo.identity != 'user'"
+        :data="retailerInfo"
+        :loginUser="loginUser"
+        module="store"
+        :on-close="closeShareSuccess">
+      </share-success>
+    </template>
   </div>
 </template>
 
@@ -151,6 +156,7 @@ import Bargainbuyitemplate from '@/components/Bargainbuyitemplate'
 import Productitemplate from '@/components/Productitemplate'
 import Newsitemplate from '@/components/Newsitemplate'
 import ShareSuccess from '@/components/ShareSuccess'
+import Sos from '@/components/Sos'
 import Time from '#/time'
 import ENV from 'env'
 import { User } from '#/storage'
@@ -160,7 +166,7 @@ export default {
     TransferDom
   },
   components: {
-    Swiper, Popup, Groupbuyitemplate, Bargainbuyitemplate, Productitemplate, Newsitemplate, ShareSuccess, XImg
+    Swiper, Popup, Groupbuyitemplate, Bargainbuyitemplate, Productitemplate, Newsitemplate, ShareSuccess, XImg, Sos
   },
   filters: {
     dateformat: function (value) {
@@ -169,6 +175,9 @@ export default {
   },
   data () {
     return {
+      showSos: false,
+      sosTitle: '',
+      showContainer: false,
       module: 'store',
       query: Object,
       loginUser: Object,
@@ -373,39 +382,50 @@ export default {
       self.$vux.loading.hide()
       self.hideloading = true
       let data = res.data
-      self.retailerInfo = data.data ? data.data : data
-      document.title = self.retailerInfo.title
-      self.wid = self.retailerInfo.uid
-      self.$util.handleWxShare({
-        module: 'store',
-        moduleid: self.wid,
-        lastshareuid: self.query.share_uid,
-        title: self.retailerInfo.title,
-        desc: self.retailerInfo.title,
-        photo: self.retailerInfo.avatar,
-        link: `${ENV.Host}/#/store?wid=${self.wid}&share_uid=${self.loginUser.uid}`,
-        successCallback: function () {
-          self.showShareSuccess = true
+      if (data.flag !== 1) {
+        self.sosTitle = data.error
+        self.showSos = true
+        return false
+      } else {
+        self.showContainer = true
+        self.retailerInfo = data.data ? data.data : data
+        document.title = self.retailerInfo.title
+        self.wid = self.retailerInfo.uid
+        self.$util.handleWxShare({
+          module: 'store',
+          moduleid: self.wid,
+          lastshareuid: self.query.share_uid,
+          title: self.retailerInfo.title,
+          desc: self.retailerInfo.title,
+          photo: self.retailerInfo.avatar,
+          link: `${ENV.Host}/#/store?wid=${self.wid}&share_uid=${self.loginUser.uid}`,
+          successCallback: function () {
+            self.showShareSuccess = true
+          }
+        })
+        if (self.retailerInfo.uid !== self.loginUser.uid) {
+          self.getCollectStaus()
         }
-      })
-      if (self.retailerInfo.uid !== self.loginUser.uid) {
-        self.getCollectStaus()
+        return self.$http.post(`${ENV.BokaApi}/api/common/topShow`, { wid: self.query.wid })
       }
-      return self.$http.post(`${ENV.BokaApi}/api/common/topShow`, { wid: self.query.wid })
     }).then(function (res) {
-      let data = res.data
-      let retdata = data.data ? data.data : data
-      for (let i = 0; i < retdata.length; i++) {
-        let p = retdata[i]
-        p.img = p.photo
-        p.url = `/product?id=${p.moduleid}&wid=${self.retailerInfo.uid}`
+      if (res) {
+        let data = res.data
+        let retdata = data.data ? data.data : data
+        for (let i = 0; i < retdata.length; i++) {
+          let p = retdata[i]
+          p.img = p.photo
+          p.url = `/product?id=${p.moduleid}&wid=${self.retailerInfo.uid}`
+        }
+        self.addata = retdata
+        let params = { params: { do: 'store', pagestart: 0, limit: 20, wid: self.query.wid } }
+        return self.$http.get(`${ENV.BokaApi}/api/retailer/listActivity`, params)
       }
-      self.addata = retdata
-      let params = { params: { do: 'store', pagestart: 0, limit: 20, wid: self.query.wid } }
-      return self.$http.get(`${ENV.BokaApi}/api/retailer/listActivity`, params)
     }).then(function (res) {
-      let data = res.data
-      self.activitydata = data.data ? data.data : data
+      if (res) {
+        let data = res.data
+        self.activitydata = data.data ? data.data : data
+      }
     })
     self.getdata1()
     self.getnewsdata()
