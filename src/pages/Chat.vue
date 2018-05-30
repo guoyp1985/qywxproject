4a6845bc61d16ce7dfd959023b4d1d4c8c8b2825
simon <siemon83@gmail.com>
2018-05-30 22:45:18 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -9,8 +9,8 @@
       <div class="chatlist" ref="scrollContent">
         <template v-for="(item,index) in data">
           <div v-if="index == 0" class="messages-date">{{item.dateline | dateFormat}}</div>
-          <div v-else-if="index + 1 < data.length && data[index].dateline - data[index - 1].dateline > 300" class="messages-date">{{data[index].dateline | dateFormat}}</div>
-          <div v-else-if="index + 1 == data.length && data[index].dateline - data[index - 1].dateline > 300" class="messages-date">{{data[index].dateline | dateFormat}}</div>
+          <div v-else-if="index + 1 < data.length && data[index].dateline - data[index - 1].dateline > diffseconds" class="messages-date">{{data[index].dateline | dateFormat}}</div>
+          <div v-else-if="index + 1 == data.length && data[index].dateline - data[index - 1].dateline > diffseconds" class="messages-date">{{data[index].dateline | dateFormat}}</div>
           <div :class="`chatitem ${getitemclass(item)}`">
             <router-link class="head" :to="{path: '/membersView', query: {uid: item.uid}}">
               <img :src="item.avatar">
@@ -203,6 +203,7 @@ import Time from '#/time'
 import Socket from '#/socket'
 import Voice from '#/voice'
 
+const diffseconds = 300
 let room = ''
 export default {
   directives: {
