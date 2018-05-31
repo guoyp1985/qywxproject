b096bb2a7ca649861209616f4a64dfee2fca5af6
simon <siemon83@gmail.com>
2018-05-31 23:07:59 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -382,7 +382,7 @@ export default {
       }
     },
     imageLoad (item) {
-      console.log(item.id +'>'+ minIdFlag)
+      alert(item.id +'>'+ minIdFlag)
       if (item.id > minIdFlag) {
         this.setScrollToBottom()
       } else {
