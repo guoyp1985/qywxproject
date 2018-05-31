8dc7a96e1d791ba0cb3e6a52cf927c1b2648d62b
simon <siemon83@gmail.com>
2018-05-31 23:09:36 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -382,7 +382,7 @@ export default {
       }
     },
     imageLoad (item) {
-      alert(item.id +'>'+ minIdFlag)
+      console.log(item.id +'>'+ minIdFlag)
       if (item.id > minIdFlag) {
         this.setScrollToBottom()
       } else {
@@ -711,8 +711,10 @@ export default {
     },
     setScrollToBottom () {
       this.$nextTick(() => {
-        const top = this.$refs.scrollContent.clientHeight - this.$refs.scrollContainer.$el.clientHeight
-        this.$refs.scrollContainer.reset({ top: top })
+        setTimeout(() => {
+          const top = this.$refs.scrollContent.clientHeight - this.$refs.scrollContainer.$el.clientHeight
+          this.$refs.scrollContainer.reset({ top: top })
+        }, 1)
       })
     },
     getNewsData () {
