3598855a664a82134d492fb84e43cf72127ad217
simon <siemon83@gmail.com>
2018-06-01 00:11:29 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -5,7 +5,7 @@
 */
 <template>
   <div id="chat-room" class="font14">
-    <scroller id="chat-scoller" :bounce="false" lock-x scrollbar-y use-pulldown :pulldown-config="{downContent: '查看历史消息', upContent: '查看历史消息'}" @on-pulldown-loading="loadingHistory" :height="viewHeight" class="chat-area bg-white scroll-container" ref="scrollContainer">
+    <scroller id="chat-scoller" lock-x scrollbar-y use-pulldown :pulldown-config="{downContent: '查看历史消息', upContent: '查看历史消息'}" @on-pulldown-loading="loadingHistory" :height="viewHeight" class="chat-area bg-white scroll-container" ref="scrollContainer">
       <div class="chatlist" ref="scrollContent">
         <template v-for="(item,index) in data">
           <div v-if="index == 0" class="messages-date">{{item.dateline | dateFormat}}</div>
@@ -195,8 +195,8 @@
   </div>
 </template>
 <script>
-import { Group, XTextarea, Grid, GridItem, XButton, Popup, TransferDom, Tab, TabItem, Swiper, SwiperItem, Search, XImg, CheckIcon } from 'vux'
-import Scroller from 'vue-scroller'
+import { Scroller, Group, XTextarea, Grid, GridItem, XButton, Popup, TransferDom, Tab, TabItem, Swiper, SwiperItem, Search, XImg, CheckIcon } from 'vux'
+// import Scroller from 'vue-scroller'
 import EmotionBox from '@/components/EmotionBox'
 import ENV from 'env'
 import { User } from '#/storage'
