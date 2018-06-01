a09aaf8852e1f157c05bf447457bbb530550ad0f
guoyp1985 <605034716@qq.com>
2018-06-01 11:40:55 +0800

    gypadd

--- src/pages/Redirect.vue
+++ src/pages/Redirect.vue
@@ -19,7 +19,6 @@ export default {
       const lUrl = urlParse(location.href, true)
       const code = lUrl.query.code
       const state = lUrl.query.state
-      alert(state)
       if (code) {
         self.$http.get(`${ENV.BokaApi}/api/authUser/${code}`)
         .then(
