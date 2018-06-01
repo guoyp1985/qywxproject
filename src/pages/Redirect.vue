781631960af75d832b22c621c463d52b41753fb0
guoyp1985 <605034716@qq.com>
2018-06-01 11:33:04 +0800

    gypadd

--- src/pages/Redirect.vue
+++ src/pages/Redirect.vue
@@ -29,7 +29,8 @@ export default {
                 ...user,
                 ...res.data.data
               })
-              location.replace(`${state}`)
+              location.href = state
+              // location.replace(`${state}`)
             }
           },
           error => {
