36e737ebb19fa07fc97432eae018dd6717f1b5d8
guoyp1985 <605034716@qq.com>
2018-06-05 11:40:11 +0800

    gypadd

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -303,6 +303,16 @@ export default {
         // self.disComments = true
       })
     },
+    clickProduct (event) {
+      let node = event.target
+      while (node) {
+        if (node.nodeType === 1 && node.getAttribute('class').indexOf('insertproduct') > -1) {
+          this.$router.push(node.getAttribute('linkurl'))
+          break
+        }
+        node = node.parentNode
+      }
+    },
     getData () {
       const self = this
       const id = this.query.id
@@ -348,6 +358,13 @@ export default {
       })
       .then(res => {
         if (res) {
+          if (self.reward.uid !== self.article.uploader) {
+            let items = document.querySelectorAll('.insertproduct')
+            for (let i = 0; i < items.length; i++) {
+              let cur = items[i]
+              cur.addEventListener('click', self.clickProduct)
+            }
+          }
           self.handleImg()
           const data = res.data
           if (data.flag === 1) {
