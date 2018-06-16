c3b1722a62b6b6b2ff60e80fd683ff1b8564cddd
guoyp1985 <605034716@qq.com>
2018-06-16 17:00:02 +0800

    gypadd

--- src/pages/RetailerProductlist.vue
+++ src/pages/RetailerProductlist.vue
@@ -157,9 +157,9 @@
                   <div class="t-table pr12 border-box mt15">
                     <div class="t-cell color-999 font14">
                       <div class="clamp1">售价:<span class="color-red"> {{ $t('RMB') }}{{ feeData.price }}</span></div>
+                      <div class="clamp1 mt5" v-if="feeData.fpid > 0">厂商佣金:<span class="color-red"> {{ $t('RMB') }}{{ feeData.rebatein }}</span></div>
                       <div class="clamp1 mt5">
-                          <span class="v_middle db-in">库存: {{ feeData.storage }}{{feeData.unit}}</span>
-                          <span class="v_middle db-in ml5">已售: {{ feeData.saled }}{{feeData.unit}}</span>
+                          <span class="v_middle db-in">已售: {{ feeData.saled }}{{feeData.unit}}</span>
                       </div>
                     </div>
                   </div>
