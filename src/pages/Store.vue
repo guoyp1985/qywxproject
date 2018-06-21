de27ad5f0ff4248cf54f54a3989a380a0a0812e7
guoyp1985 <605034716@qq.com>
2018-06-21 14:21:01 +0800

    gypadd

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -454,7 +454,6 @@ export default {
         if (res) {
           const data = res.data
           const retdata = data.data ? data.data : data
-          alert(JSON.stringify(retdata))
           for (let i = 0; i < retdata.length; i++) {
             let p = retdata[i]
             p.img = p.photo
