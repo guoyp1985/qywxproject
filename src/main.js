c2e2f1d88e8ae175c927ad424bf653705dc3f393
S.H <23393331@qq.com>
2018-07-07 23:45:09 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -264,13 +264,14 @@ router.afterEach(function (to) {
 
 // Token.remove()
 // let bkAccessFlag = true
-// let wxAccessFlag = true
+let wxAccessFlag = true
 // WxAccess.remove()
 const handleUserInfo = () => {
   const lUrl = urlParse(location.href, true)
   const code = lUrl.query.code
   const state = lUrl.query.state
-  if (state === 'defaultAccess' && code) {
+  if (wxAccessFlag && state === 'defaultAccess' && code) {
+    wxAccessFlag = false
     // 401授权，取得token
     Vue.http.get(`${ENV.BokaApi}/api/authLogin/${code}`)
     .then(
