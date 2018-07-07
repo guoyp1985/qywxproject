29b106c31dc708f21f1460f401f2ae10f8120e84
S.H <23393331@qq.com>
2018-07-07 23:25:55 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -345,7 +345,7 @@ const matchExclude = url => {
 Vue.http.interceptors.request.use(config => {
   if (!matchExclude(config.url)) {
     config.cancelToken = new CancelToken(c => {
-      alert(c)
+      alert(config.method)
       pendings.push({ u: config.url + '&' + config.method, f: c })
     })
     alert('TOKEN:'+token.token)
