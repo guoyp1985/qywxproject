7d0e810184be42c4c30be4bfde9c58872190a3fc
guoyp1985 <605034716@qq.com>
2018-08-11 14:38:45 +0800

    gypadd

--- src/pages/RetailerGoodeazy.vue
+++ src/pages/RetailerGoodeazy.vue
@@ -3,116 +3,119 @@
     <subscribe v-if="loginUser.subscribe != 1"></subscribe>
     <apply-tip v-if="showApply"></apply-tip>
     <template v-if="showContainer">
-      <div class="s-topbanner s-topbanner1 bg-white">
-        <div class="row">
-          <tab v-model="selectedIndex" class="v-tab">
-            <tab-item v-for="(item,index) in tabtxts" :selected="index == 0" :key="index">{{item}}</tab-item>
-          </tab>
+      <open-vip v-if="showVip && loginUser.isretailer == 2" @hide-vip="hideVip" @open-vip="openVip"></open-vip>
+      <template v-else>
+        <div class="s-topbanner s-topbanner1 bg-white">
+          <div class="row">
+            <tab v-model="selectedIndex" class="v-tab">
+              <tab-item v-for="(item,index) in tabtxts" :selected="index == 0" :key="index">{{item}}</tab-item>
+            </tab>
+          </div>
         </div>
-      </div>
-      <div class="s-container s-container1">
-        <swiper v-model="selectedIndex" class="x-swiper no-indicator" @on-index-change="swiperChange">
-          <swiper-item :class="`swiperitem scroll-container${index}`" v-for="(tabitem, index) in tabtxts" :key="index">
-            <div v-if="(index == 0)" class="swiper-inner scroll-container1" ref="scrollContainer1" @scroll="handleScroll1">
-              <div class="font15 pt15 pl10 pr10">搜索关键词采集文章</div>
-              <div class="font12 color-gray mt5 pl10 pr10">在搜索框内输入文章关键词，点击“搜索”按钮搜索相关文章后，即可预览或采集文章素材。</div>
-              <div class="mb15" style="position:relative;">
-                <search
-                  class="v-search bg-white"
-                  v-model="searchword"
-                  :auto-fixed="autofixed"
-                  @on-submit="onSubmit"
-                  @on-change="onChange"
-                  @on-cancel="onCancel"
-                  ref="search">
-                </search>
-                <checker
-                v-if="keywordsData.length > 0"
-                class="v-checker pt10 pl10 pr10"
-                type="radio"
-                v-model="keyword"
-                default-item-class="ck-item"
-                selected-item-class="ck-item-selected"
-                @on-change="searchEvent">
-                  <checker-item class="border1px color-gray" v-for="(kw, keyindex) in keywordsData" :key="keyindex" :value="kw">{{ kw }}</checker-item>
-                </checker>
-                <div class="scroll_list pl10 pr10 mb12">
-                  <div v-if="showSearchEmpty && (!searchdata || searchdata.length == 0)" class="scroll_item emptyitem">
-                    <div class="t-table">
-                      <div class="t-cell">暂无搜索结果</div>
+        <div class="s-container s-container1">
+          <swiper v-model="selectedIndex" class="x-swiper no-indicator" @on-index-change="swiperChange">
+            <swiper-item :class="`swiperitem scroll-container${index}`" v-for="(tabitem, index) in tabtxts" :key="index">
+              <div v-if="(index == 0)" class="swiper-inner scroll-container1" ref="scrollContainer1" @scroll="handleScroll1">
+                <div class="font15 pt15 pl10 pr10">搜索关键词采集文章</div>
+                <div class="font12 color-gray mt5 pl10 pr10">在搜索框内输入文章关键词，点击“搜索”按钮搜索相关文章后，即可预览或采集文章素材。</div>
+                <div class="mb15" style="position:relative;">
+                  <search
+                    class="v-search bg-white"
+                    v-model="searchword"
+                    :auto-fixed="autofixed"
+                    @on-submit="onSubmit"
+                    @on-change="onChange"
+                    @on-cancel="onCancel"
+                    ref="search">
+                  </search>
+                  <checker
+                  v-if="keywordsData.length > 0"
+                  class="v-checker pt10 pl10 pr10"
+                  type="radio"
+                  v-model="keyword"
+                  default-item-class="ck-item"
+                  selected-item-class="ck-item-selected"
+                  @on-change="searchEvent">
+                    <checker-item class="border1px color-gray" v-for="(kw, keyindex) in keywordsData" :key="keyindex" :value="kw">{{ kw }}</checker-item>
+                  </checker>
+                  <div class="scroll_list pl10 pr10 mb12">
+                    <div v-if="showSearchEmpty && (!searchdata || searchdata.length == 0)" class="scroll_item emptyitem">
+                      <div class="t-table">
+                        <div class="t-cell">暂无搜索结果</div>
+                      </div>
                     </div>
-                  </div>
-                  <div v-else v-for="(item,index) in searchdata" :key="item.id" class="scroll_item pt10 pb10">
-                    <div class="t-table">
-                      <a :href="item.url" class="t-cell v_middle">
-                        <div class="clamp1">{{ item.title }}</div>
-                        <div class="clamp2 font12 color-gray mt5">{{ item.summary }}</div>
-                      </a>
-                      <div class="t-cell align_right v_middle w60">
-                        <div class="qbtn bg-red color-white" @click="collect(item,index)">{{ $t('Collect') }}</div>
+                    <div v-else v-for="(item,index) in searchdata" :key="item.id" class="scroll_item pt10 pb10">
+                      <div class="t-table">
+                        <a :href="item.url" class="t-cell v_middle">
+                          <div class="clamp1">{{ item.title }}</div>
+                          <div class="clamp2 font12 color-gray mt5">{{ item.summary }}</div>
+                        </a>
+                        <div class="t-cell align_right v_middle w60">
+                          <div class="qbtn bg-red color-white" @click="collect(item,index)">{{ $t('Collect') }}</div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div v-if="(index == 1)" class="swiper-inner scroll-container2" ref="scrollContainer2" @scroll="handleScroll2">
-              <div class="pl10 pr10">
-                <div class="font15 pt15">文章链接采集文章</div>
-                <div class="font12 color-gray mt5">请从微信公众号中复制文章链接，粘贴在文本框内，点击“采集”按钮，采集成功后即可编辑分享</div>
-                <form class="subform mb15">
-                  <div class="flex_left mt12 border1px">
-                    <div class="flex_cell" style="height:100%;">
-                      <group class="textarea-outer">
-                        <x-textarea
-                          ref="urlTextarea"
-                          v-model="collecturl"
-                          class="x-textarea noborder"
-                          name="url"
-                          :placeholder="$t('Url paster here')"
-                          :show-counter="false"
-                          :rows="1"
-                          @on-change="textareaChange('urlTextarea')"
-                          @on-focus="textareaFocus('urlTextarea')"
-                          autosize>
-                        </x-textarea>
-                      </group>
+              <div v-if="(index == 1)" class="swiper-inner scroll-container2" ref="scrollContainer2" @scroll="handleScroll2">
+                <div class="pl10 pr10">
+                  <div class="font15 pt15">文章链接采集文章</div>
+                  <div class="font12 color-gray mt5">请从微信公众号中复制文章链接，粘贴在文本框内，点击“采集”按钮，采集成功后即可编辑分享</div>
+                  <form class="subform mb15">
+                    <div class="flex_left mt12 border1px">
+                      <div class="flex_cell" style="height:100%;">
+                        <group class="textarea-outer">
+                          <x-textarea
+                            ref="urlTextarea"
+                            v-model="collecturl"
+                            class="x-textarea noborder"
+                            name="url"
+                            :placeholder="$t('Url paster here')"
+                            :show-counter="false"
+                            :rows="1"
+                            @on-change="textareaChange('urlTextarea')"
+                            @on-focus="textareaFocus('urlTextarea')"
+                            autosize>
+                          </x-textarea>
+                        </group>
+                      </div>
+                      <div class="align_center bg-red color-white font15 w80" style="height:45px;line-height:45px;border-radius:5px;" @click="collect1">{{ $t('Collect') }}</div>
                     </div>
-                    <div class="align_center bg-red color-white font15 w80" style="height:45px;line-height:45px;border-radius:5px;" @click="collect1">{{ $t('Collect') }}</div>
-                  </div>
-                </form>
-              </div>
-              <div class="bg-page" style="height:12px;"></div>
-              <div class="padding15 font15 b_bottom_after">{{ $t('Collect record') }}</div>
-              <div v-if="disNewslist" class="scroll_list pl10 pr10 pb10">
-                <div v-if="!newsdata || newsdata.length == 0" class="scroll_item emptyitem">
-                  <div class="t-table">
-                    <div class="t-cell">您还没有采集过文章</div>
-                  </div>
+                  </form>
                 </div>
-                <router-link v-else v-for="(item,index) in newsdata" :key="item.id" class="scroll_item pt10 pb10 db" :to="{path: '/news', query: {id: item.id}}">
-                  <div class="flex_left">
-                    <div class="">
-                      <img class="imgcover v_middle avatarimg1 radius0" :src="item.photo" onerror="javascript:this.src='http://vuxlaravel.boka.cn/images/nopic.jpg';" />
-                    </div>
-                    <div class="flex_cell pl10">
-                      <div class="clamp1 font14">{{item.title}}</div>
-                      <div class="clamp1 font12 color-gray">{{ item.dateline | dateformat }}</div>
+                <div class="bg-page" style="height:12px;"></div>
+                <div class="padding15 font15 b_bottom_after">{{ $t('Collect record') }}</div>
+                <div v-if="disNewslist" class="scroll_list pl10 pr10 pb10">
+                  <div v-if="!newsdata || newsdata.length == 0" class="scroll_item emptyitem">
+                    <div class="t-table">
+                      <div class="t-cell">您还没有采集过文章</div>
                     </div>
                   </div>
-                </router-link>
+                  <router-link v-else v-for="(item,index) in newsdata" :key="item.id" class="scroll_item pt10 pb10 db" :to="{path: '/news', query: {id: item.id}}">
+                    <div class="flex_left">
+                      <div class="">
+                        <img class="imgcover v_middle avatarimg1 radius0" :src="item.photo" onerror="javascript:this.src='http://vuxlaravel.boka.cn/images/nopic.jpg';" />
+                      </div>
+                      <div class="flex_cell pl10">
+                        <div class="clamp1 font14">{{item.title}}</div>
+                        <div class="clamp1 font12 color-gray">{{ item.dateline | dateformat }}</div>
+                      </div>
+                    </div>
+                  </router-link>
+                </div>
               </div>
-            </div>
-          </swiper-item>
-        </swiper>
-      </div>
-      <div class="s-bottom bottomnaviarea b_top_after">
-        <div class="t-table bottomnavi">
-          <router-link class="t-cell item" :to="{path: '/store', query: {wid: loginUser.uid}}">{{ $t('My shop') }}</router-link>
-          <router-link class="t-cell item" to="/centerSales">{{ $t('Sales center') }}</router-link>
-          <router-link class="t-cell item" to="/retailerOrders">{{ $t('My orders') }}</router-link>
+            </swiper-item>
+          </swiper>
         </div>
-      </div>
+        <div class="s-bottom bottomnaviarea b_top_after">
+          <div class="t-table bottomnavi">
+            <router-link class="t-cell item" :to="{path: '/store', query: {wid: loginUser.uid}}">{{ $t('My shop') }}</router-link>
+            <router-link class="t-cell item" to="/centerSales">{{ $t('Sales center') }}</router-link>
+            <router-link class="t-cell item" to="/retailerOrders">{{ $t('My orders') }}</router-link>
+          </div>
+        </div>
+      </template>
     </template>
   </div>
 </template>
@@ -127,10 +130,11 @@ import ENV from 'env'
 import { User } from '#/storage'
 import Sos from '@/components/Sos'
 import Subscribe from '@/components/Subscribe'
+import OpenVip from '@/components/OpenVip'
 
 export default {
   components: {
-    Tab, TabItem, Swiper, SwiperItem, Search, XTextarea, Group, Checker, CheckerItem, XImg, Sos, Subscribe
+    Tab, TabItem, Swiper, SwiperItem, Search, XTextarea, Group, Checker, CheckerItem, XImg, Sos, Subscribe, OpenVip
   },
   filters: {
     dateformat: function (value) {
@@ -159,10 +163,17 @@ export default {
       pagestart1: 0,
       limit1: 10,
       clickSearchword: '',
-      historyLimit: 15
+      historyLimit: 15,
+      showVip: false
     }
   },
   methods: {
+    hideVip () {
+      this.$router.go(-1)
+    },
+    openVip () {
+      location.replace(`${ENV.Host}/#/pay?id=${this.loginUser.payorderid}&module=payorders`)
+    },
     textareaChange (refname) {
       let curArea = this.$refs[refname][0] ? this.$refs[refname][0] : this.$refs[refname]
       curArea.updateAutosize()
@@ -173,20 +184,29 @@ export default {
     },
     handleScroll2 () {
       const self = this
-      self.$util.scrollEvent({
-        element: self.$refs.scrollContainer2[0],
-        callback: function () {
-          if (self.newsdata.length === (self.pagestart + 1) * self.limit) {
-            self.pagestart++
-            self.$vux.loading.show()
-            self.getnewsdata()
+      if (self.loginUser.isretailer === 1) {
+        self.showVip = false
+        self.$util.scrollEvent({
+          element: self.$refs.scrollContainer2[0],
+          callback: function () {
+            if (self.newsdata.length === (self.pagestart + 1) * self.limit) {
+              self.pagestart++
+              self.$vux.loading.show()
+              self.getnewsdata()
+            }
           }
-        }
-      })
+        })
+      }
     },
     getnewsdata () {
       this.$vux.loading.show()
       const self = this
+      if (self.loginUser.isretailer === 2) {
+        self.limit = 5
+      } else if (self.loginUser.isretailer === 1) {
+        self.showVip = false
+        self.limit = 10
+      }
       const params = { do: 'list', pagestart: self.pagestart, limit: self.limit }
       this.$http.post(`${ENV.BokaApi}/api/news/goodeazy`, params)
       .then(res => {
@@ -195,6 +215,11 @@ export default {
         self.$vux.loading.hide()
         self.newsdata = self.newsdata.concat(retdata)
         self.disNewslist = true
+        if (self.loginUser.isretailer === 2) {
+          if (self.newsdata.length >= 5) {
+            self.showVip = true
+          }
+        }
       })
     },
     getHistoryData () {
@@ -291,27 +316,32 @@ export default {
     },
     collect (item, index) {
       const self = this
-      self.$vux.confirm.show({
-        content: '确定要采集该文章吗？',
-        onConfirm () {
-          self.$vux.loading.show()
-          self.$http.post(`${ENV.BokaApi}/api/news/goodeazy`,
-            { do: 'download', url: item.url }
-          ).then(function (res) {
-            const data = res.data
-            self.$vux.loading.hide()
-            self.$vux.toast.show({
-              text: data.error,
-              time: self.$util.delay(data.error),
-              onHide: function () {
-                if (data.flag === 1) {
-                  self.$router.push({path: '/news', query: {id: data.data.id}})
+      if (self.newsdata.length >= 5 && self.loginUser.isretailer === 2) {
+        self.showVip = true
+      } else if (self.loginUser.isretailer === 1) {
+        self.showVip = false
+        self.$vux.confirm.show({
+          content: '确定要采集该文章吗？',
+          onConfirm () {
+            self.$vux.loading.show()
+            self.$http.post(`${ENV.BokaApi}/api/news/goodeazy`,
+              { do: 'download', url: item.url }
+            ).then(function (res) {
+              const data = res.data
+              self.$vux.loading.hide()
+              self.$vux.toast.show({
+                text: data.error,
+                time: self.$util.delay(data.error),
+                onHide: function () {
+                  if (data.flag === 1) {
+                    self.$router.push({path: '/news', query: {id: data.data.id}})
+                  }
                 }
-              }
+              })
             })
-          })
-        }
-      })
+          }
+        })
+      }
     },
     collect1 () {
       const self = this
@@ -391,6 +421,7 @@ export default {
           self.initContainer()
           self.showContainer = true
           this.query = this.$route.query
+          this.getnewsdata()
         }
         // }
       }
