2d14b315545abc06595e329608091eb4152e0f25
guoyp1985 <605034716@qq.com>
2018-08-30 14:57:25 +0800

    gypadd

--- src/pages/MpSetting.vue
+++ src/pages/MpSetting.vue
@@ -82,86 +82,66 @@
     </div>
     <div class="boxitem">
       <div class="title">开发版本</div>
-      <div class="db-flex b_bottom_after pb20">
-        <div class="col1">
-          <div>
-            <div class="gray">版本号</div>
-            <div class="font22">0.02</div>
+      <template v-if="disDevelopmentData">
+        <div v-if="!developmentData.length" class="pt20 pb20 flex_left">暂无上传开发数据</div>
+        <div v-else class="data-list">
+          <div class="db-flex b_bottom_after pb20 data-item" v-for="(item,index) in developmentData" :key="index">
+            <div class="col1">
+              <div>
+                <div class="gray">版本号</div>
+                <div class="font22">{{item.user_version}}</div>
+              </div>
+            </div>
+            <div class="col2 flex_cell">
+              <div class="flex_left">
+                <div class="gray w100">开发者</div>
+                <div class="flex_cell">{{item.developer}}</div>
+              </div>
+              <div class="flex_left mt12">
+                <div class="gray w100">提交时间</div>
+                <div class="flex_cell">{{item.create_time | dateFormat}}</div>
+              </div>
+              <div class="flex_left mt12">
+                <div class="gray w100">描述</div>
+                <div class="flex_cell">{{item.user_desc}}</div>
+              </div>
+            </div>
+            <div class="col3">
+              <div class="btn">详情</div>
+              <div class="btn">提交审核</div>
+            </div>
+            <div class="col4">
+              <div class="btn" @click="clickBtn3"><i class="al al-jiantou_down"></i></div>
+              <div v-if="showBtn3" class="btnlayer">
+                <div class="item">选为体验版本</div>
+                <div class="item">删除</div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col2 flex_cell">
-          <div class="flex_left">
-            <div class="gray w100">开发者</div>
-            <div>Theoneจุ๊บ</div>
-          </div>
-          <div class="flex_left mt12">
-            <div class="gray w100">提交时间</div>
-            <div>2018-07-11 14:49:00</div>
-          </div>
-          <div class="flex_left mt12">
-            <div class="gray w100">描述</div>
-            <div>Theoneจุ๊บ 在 7/27/2018 5:56:56 PM 提交上传</div>
-          </div>
-        </div>
-        <div class="col3">
-          <div class="btn">详情</div>
-          <div class="btn">提交审核</div>
-        </div>
-        <div class="col4">
-          <div class="btn" @click="clickBtn3"><i class="al al-jiantou_down"></i></div>
-          <div v-if="showBtn3" class="btnlayer">
-            <div class="item">选为体验版本</div>
-            <div class="item">删除</div>
-          </div>
-        </div>
-      </div>
-      <div class="db-flex pt20">
-        <div class="col1">
-          <div>
-            <div class="gray">版本号</div>
-            <div class="font22">0.02</div>
-          </div>
-        </div>
-        <div class="col2 flex_cell">
-          <div class="flex_left">
-            <div class="gray w100">开发者</div>
-            <div>Theoneจุ๊บ</div>
-          </div>
-          <div class="flex_left mt12">
-            <div class="gray w100">提交时间</div>
-            <div>2018-07-11 14:49:00</div>
-          </div>
-          <div class="flex_left mt12">
-            <div class="gray w100">描述</div>
-            <div>Theoneจุ๊บ 在 7/27/2018 5:56:56 PM 提交上传</div>
-          </div>
-        </div>
-        <div class="col3">
-          <div class="btn">详情</div>
-          <div class="btn">提交审核</div>
-        </div>
-        <div class="col4">
-          <div class="btn" @click="clickBtn4"><i class="al al-jiantou_down"></i></div>
-          <div v-if="showBtn4" class="btnlayer">
-            <div class="item">取消体验</div>
-            <div class="item">修改页面路径</div>
-            <div class="item">删除</div>
-          </div>
-        </div>
-      </div>
+      </template>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import Time from '#/time'
+import ENV from 'env'
 export default {
+  filters: {
+    dateFormat: function (value) {
+      return new Time(value * 1000).dateFormat('yyyy-MM-dd hh:mm')
+    }
+  },
   data () {
     return {
       showBtn1: false,
       showBtn2: false,
       showBtn3: false,
-      showBtn4: false
+      showBtn4: false,
+      developmentData: [],
+      disDevelopmentData: false
     }
   },
   methods: {
@@ -204,6 +184,12 @@ export default {
       }
     },
     refresh () {
+      const self = this
+      self.$http.get(`${ENV.BokaApi}/api/open/getTemplates`).then(function (res) {
+        let data = res.data
+        self.developmentData = data.data ? data.data : data
+        self.disDevelopmentData = true
+      })
     }
   },
   activated () {
@@ -250,4 +236,6 @@ export default {
 .boxlist .btnlayer.active{display:block;}
 .btnlayer .item{min-width:90px;padding: 0 22px;line-height:40px;color:#353535;}
 .btnlayer .item:not(:last-child){border-bottom: 1px solid #e7e7eb;}
+
+.data-list .data-item:not(:first-child){padding-top:20px;}
 </style>
