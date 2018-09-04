25bbd2ee7e5973915b8f2b78938ecd2c841fd89f
guoyp1985 <605034716@qq.com>
2018-09-04 15:23:01 +0800

    gypadd

--- src/pages/MpSetting.vue
+++ src/pages/MpSetting.vue
@@ -266,6 +266,8 @@ export default {
         let showError = data.error
         if (data.error.search(/user already bind hint/g) > -1) {
           showError = '该用户已绑定过体验者'
+        } else if (data.error.search(/user not exist or user cannot be searched hint/g) > -1) {
+          showError = '无法搜索到该微信号，请确保微信号输入正确'
         }
         if (data.flag) {
           self.$vux.toast.text(showError, 'middle')
