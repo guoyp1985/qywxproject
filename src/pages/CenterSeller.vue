647ef9c55ffb0ba8181c71c3fe3fa9f2dd063ae0
guoyp1985 <605034716@qq.com>
2018-09-06 16:26:26 +0800

    gypadd

--- src/pages/CenterSeller.vue
+++ src/pages/CenterSeller.vue
@@ -56,11 +56,13 @@
               </div>
               <div class="row2" v-if="(userInfo.slogan && userInfo.slogan != '') || userInfo.uid == loginUser.uid">
                 <span class="v_middle color-red5">{{$t('Seller said')}}: </span>
-                <span class="v_middle" v-html="userInfo.slogan"></span>
+                <router-link v-if="(!userInfo.slogan || userInfo.slogan == '') && userInfo.uid == loginUser.uid" :to="{path:'/retailerSetting'}" class="color-gray">点击此处设置</router-link>
+                <span v-else class="v_middle" v-html="userInfo.slogan"></span>
               </div>
               <div class="row3" v-if="(userInfo.tags && userInfo.tags.length > 0) || userInfo.uid == loginUser.uid">
                 <span class="v_middle color-red5">{{$t('Seller tags')}}: </span>
-                <div class="taglist">
+                <router-link v-if="!userInfo.tags.length" :to="{path:'/retailerSetting'}" class="color-gray">点击此处设置</router-link>
+                <div v-else class="taglist">
                   <div class="tagitem" @click="clickTag(item)" v-for="(item,index) in userInfo.tags">{{item.title}}({{item.timelines}})</div>
                 </div>
               </div>
