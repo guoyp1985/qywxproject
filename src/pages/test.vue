21cd1658241dcdc554a12d67297bea9494970270
guoyp1985 <605034716@qq.com>
2018-10-27 13:27:10 +0800

    gypadd

---
+++ src/pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="containerarea flex_center bold font20">测试授权
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    init () {
+      this.$util.wxAccessListening()
+      this.$util.wxAccess()
+    }
+  },
+  created () {
+    this.init()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
