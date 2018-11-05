dfd038600e7a4ee731e9927300cbca44f53743e2
guoyp1985 <605034716@qq.com>
2018-11-05 11:25:09 +0800

    gypadd

--- src/components/OrderInfo.vue
+++ src/components/OrderInfo.vue
@@ -47,7 +47,7 @@
           <span class="v_middle font12 color-gray" v-else>( {{ $t('Postage') }}: {{ $t('RMB') }}{{ item.postage }} )</span>
         </template>
         <template v-if="item.carddeduct > 0">
-          <span class="v_middle font12 color-gray">{{ $t('RMB') }}{{ item.carddeduct }}</span>
+          <span class="v_middle font12 color-gray">优惠券抵扣: {{ $t('RMB') }}{{ item.carddeduct }}</span>
         </template>
       </div>
     </div>
