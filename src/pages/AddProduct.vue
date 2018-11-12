9901102eb9219c9bd6d72fb27c9520a09120cd41
simon <siemon83@gmail.com>
2018-11-12 19:25:48 +0800

    upload by simon

--- src/pages/AddProduct.vue
+++ src/pages/AddProduct.vue
@@ -391,7 +391,7 @@ export default {
     uploadPhoto (refname, type) {
       const self = this
       const fileInput = self.$refs[refname][0] ? self.$refs[refname][0] : self.$refs[refname]
-      if (self.$util.isAndroid() || self.$util.isPC() || type === 'video') {
+      if (self.$util.isPC() || type === 'video') {
         fileInput.click()
       } else {
         self.$wechat.ready(function () {
