87fe05529dabac13a53b1f1e8c9969f76a45f689
simon <siemon83@gmail.com>
2018-11-13 12:12:26 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -384,10 +384,10 @@ export default {
       if (this.showVoiceCom) {
         this.showVoiceCom = false
       } else {
-        this.setScrollToBottom(false)
         this.checkRecordApi()
         this.showVoiceCom = true
       }
+      this.setScrollToBottom(false)
     },
     toggleEmotion () {
       const self = this
