29091d1ec31039f4802df6c14614929495b3be60
S.H <23393331@qq.com>
2018-11-19 17:55:24 +0800

    upload by simon

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -370,16 +370,16 @@ export default {
       const self = this
       const globalContianer = document.getElementById('vux_view_box_body')
       this.showFeatureBox = false
-      // intervalId = setInterval(function () {
-      //   document.body.scrollTop = document.body.scrollHeight
-      // }, 100)
+      intervalId = setInterval(function () {
+        document.body.scrollTop = document.body.scrollHeight
+      }, 100)
       let text = this.$refs.text[0] ? this.$refs.text[0] : this.$refs.text
       text.updateAutosize()
       this.setScrollToBottom(false)
-      if (self.$util.isAndroid()) {
+      if (this.$util.isAndroid()) {
         globalContianer.scrollTop = globalContianer.scrollHeight
-        const top = self.$refs.scrollContent.clientHeight - self.$refs.scrollContainer.$el.clientHeight
-        self.$refs.scrollContainer.reset({ top: top + 52 })
+        const top = this.$refs.scrollContent.clientHeight - this.$refs.scrollContainer.$el.clientHeight
+        this.$refs.scrollContainer.reset({ top: top + 52 })
       }
     },
     onBlur () {
