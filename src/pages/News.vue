0ffec7685b36c3f0303ed09000c74c56db0b317f
guoyp1985 <605034716@qq.com>
2018-12-12 18:37:02 +0800

    gypadd

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -29,8 +29,10 @@
           </div>
           <div class="article-info" style="position:relative;">
             <span class="article-date color-gray">{{article.dateline | dateFormat}}</span>
-            <span v-if="reward.subscribe != 1 && query.control != 'edit'" @click="popupSubscribe" class="article-ex color-blue">{{ WeixinName }}</span>
-            <router-link v-else to="/subscribeInfo" class="article-ex color-blue">{{ WeixinName }}</router-link>
+            <template v-if="query.control != 'edit'">
+              <span v-if="reward.subscribe != 1" @click="popupSubscribe" class="article-ex color-blue">{{ WeixinName }}</span>
+              <router-link v-else to="/subscribeInfo" class="article-ex color-blue">{{ WeixinName }}</router-link>
+            </template>
             <router-link class="article-author" :to="{ name: '', params: {} }">{{article.author}}</router-link>
             <div v-if="retailerInfo.uid" class="align_right" style="position:absolute;right:0;top:50%;margin-top:-12px;">
               <div @click="onStore" class="qbtn4 font12" style="padding:1px 8px;">{{ retailerInfo.title }}</div>
