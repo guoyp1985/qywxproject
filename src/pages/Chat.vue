5f28cefb68c9f5626257302ac81375a01e2c47b2
guoyp1985 <605034716@qq.com>
2018-12-25 18:33:55 +0800

    gypadd

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -6,7 +6,7 @@
 <template>
   <div id="chat-room" class="font14">
     <template v-if="allowChat || loginUser.isretailer === 1">
-      <div style="opacity:0;position:absolute;width:0;overflow:hidden;" class="copy_txt" v-html="filterEmot(clickMsgItem.content)"></div>
+      <div style="opacity:0;position:absolute;width:0;overflow:hidden;" class="copy_txt">{{clickMsgItem.content}}</div>
       <div v-if="retailerInfo.uid && showTip" ref="topTipArea" class="db-flex w_100 border-box padding10 bg-white b_bottom_after font13 color-gray" @click="toStore" style="color:inherit;">
         <div class="flex_left" style="width:70px;">
           <img class="v_middle imgcover" style="width:60px;height:60px;" :src="retailerInfo.avatar" onerror="javascript:this.src='https://tossharingsales.boka.cn/images/user.jpg';" />
