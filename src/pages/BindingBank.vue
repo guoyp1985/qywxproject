0e9399c43144ffc672d19f96aa92c6eaf93ba12f
guoyp1985 <605034716@qq.com>
2018-12-27 16:55:13 +0800

    gypadd

--- src/pages/BindingBank.vue
+++ src/pages/BindingBank.vue
@@ -38,7 +38,14 @@
     <div class="flex_center color-white btn-bottom-red mt20" @click="bindEvent">绑定</div>
     <div class="mt20 padding20">
       <div class="bold">*注意事项</div>
-      <div>确保姓名填写正确</div>
+      <div>1. 请确保持卡人姓名为本人姓名</div>
+      <div>2. 请认真核对银行卡号是否正确</div>
+      <div>3. 目前支持17家银行，更多银行逐步开放</div>
+      <div class="bold mt20">*银行卡提现规则</div>
+      <div>1. 银行卡每日提现额度为2万元</div>
+      <div>2. 每笔提现手续费为提现金额的0.1%，最低1元，最高25元。</div>
+      <div>3. 提现至银行卡到账时间为1-3日内，具体以银行到账时间为准。</div>
+      <div>4. 若1-3日内提现未到账，请核对您的持卡人姓名、银行卡号、所属银行是否正确，更正后重新提现即可。</div>
     </div>
     <div v-transfer-dom class="x-popup">
       <popup v-model="showpopup" height="100%">
