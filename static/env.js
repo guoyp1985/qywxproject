ae5e0a66df143ca7d883bc986b7b0c4a4464410f
guoyp1985 <605034716@qq.com>
2019-01-04 11:59:11 +0800

    gypadd

--- static/env.js
+++ static/env.js
@@ -22,7 +22,7 @@ var ENV = {
   ],
   MiniRouter: {
     contentsNews: '/pages/contentsNews',
-    knowledgeList: '/pages/contentsKnowledge',
+    knowledgeList: '/package/pages/contentsKnowledge',
     news: '/packageB/pages/news',
     pay: '/packageB/pages/pay',
     product: '/packageB/pages/product',
