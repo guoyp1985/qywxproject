10abf8225ee30ebc2038bb42144850957356009c
1 <git@boka.cn>
2019-02-13 16:12:10 +0800

    ymladd

--- src/pages/RoomApply.vue
+++ src/pages/RoomApply.vue
@@ -68,7 +68,9 @@
         <check-icon :value.sync="isAccept">同意<a class="color-red" @click.stop="showProtocol">群群推协议</a></check-icon>
       </div>
       <div class="flex_center btnin">
-        <input type="text" v-model="crypto" class="inputs ml20" placeholder="请输入密钥"></input>
+        <div class="inputs ml20">
+          <input type="text" v-model="crypto" placeholder="请输入密钥"></input>
+        </div>
         <button v-if="isAccept == true" @click="submitHandle" :class="`${rgbred}`">验证</button>
         <button v-else disabled="true">验证</button>
       </div>
@@ -159,8 +161,9 @@ export default {
 .rgba09red{background-color:#FB5657 !important;}
 .btnin{
   .inputs{
-    border:1px solid #C3C3C3;height:31px;padding-left:10px;
+    border:1px solid #C3C3C3;height:31px;padding-left:10px;width:150px;
   }
+  input{outline:none;margin-top:9px;width:120px;}
   button{
     width:90px;height:33px;text-align:center;line-height:33px;color:#fff;background-color:#C3C3C3;border:0;
     border-top-right-radius:5px;border-bottom-right-radius:5px;margin-left: -1px;
