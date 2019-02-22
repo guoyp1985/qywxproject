2bbe8427a8b127fd01f1587e3ac0aa5d806227f1
guoyp1985 <605034716@qq.com>
2019-02-22 16:47:17 +0800

    gypadd

--- src/pages/FactoryList.vue
+++ src/pages/FactoryList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="containerarea bg-page font14 factory-list-page nobottom notop">
     <apply-tip v-if="showApply"></apply-tip>
-    <subscribe v-if="loginUser.subscribe != 1"></subscribe>
+    <subscribe v-if="loginUser.subscribe != 1 && !loginUser.isretailer"></subscribe>
     <template v-if="showContainer">
       <div class="pagemiddle" ref="scrollContainer" @scroll="handleScroll('scrollContainer', 0)">
         <template v-if="disTabData1">
@@ -184,14 +184,7 @@ export default {
       this.$store.commit('updateToggleTabbar', {toggleTabbar: false})
       this.$vux.loading.show()
       this.loginUser = User.get()
-      if (this.loginUser && this.loginUser.subscribe === 1) {
-        // if (self.loginUser.isretailer === 2) {
-        //   this.$vux.loading.hide()
-        //   self.initContainer()
-        //   self.$vux.loading.hide()
-        //   let backUrl = encodeURIComponent(location.href)
-        //   location.replace(`${ENV.Host}/#/pay?id=${self.loginUser.payorderid}&module=payorders&lasturl=${backUrl}`)
-        // }
+      if (this.loginUser && (this.loginUser.subscribe === 1 || this.loginUser.isretailer)) {
         if (!this.loginUser.isretailer) {
           this.$vux.loading.hide()
           self.initContainer()
