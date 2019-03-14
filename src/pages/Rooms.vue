d2d369e9a20ae6025d76630fa1ca43e117994cf8
guoyp1985 <605034716@qq.com>
2019-03-14 15:27:20 +0800

    gypadd

--- src/pages/Rooms.vue
+++ src/pages/Rooms.vue
@@ -46,7 +46,7 @@
               </span>
             </div>
             <div slot="operation-area" class="room-operate-area db-flex">
-              <router-link class="flex_cell font13 button" :to="{ name: 'tRoomDetails', query: {id: item.id} }">了解更多</router-link>
+              <div class="flex_cell font13 button" @click="toRoomDetails(item)">了解更多</div>
               <div class="flex_cell font13 button" @click="toOrderDeal(item)">与TA交易</div>
             </div>
           </room-view>
@@ -99,12 +99,12 @@ export default {
       }
       this.$router.push({path: '/roomOrders', query: params})
     },
-    toOrderDeal (item) {
+    toRoomDetails (item) {
       let params = {id: item.id}
       if (this.query.from) {
         params.from = this.query.from
       }
-      this.$router.push({path: '/roomOrderDeal', query: params})
+      this.$router.push({path: '/roomDetails', query: params})
     },
     toStart () {
       this.$router.push('/roomStart?frompage=rooms')
