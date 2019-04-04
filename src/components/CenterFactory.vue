4787f4ddac90272fcddd8e4ce6190fb66a95a241
1 <git@boka.cn>
2019-04-04 17:36:50 +0800

    ymladd

--- src/components/CenterFactory.vue
+++ src/components/CenterFactory.vue
@@ -15,7 +15,7 @@
         <div class="txt-cell pl10">
           <div class="font17 color-white">{{ factoryInfo.title }}</div>
           <div class="font13 color-white mt5">{{ factoryInfo.summary }}</div>
-          <div class="font13 mt5" style="color:#f1f78f;" @click="showToast">厂家使用期至：2019-5-2</div>
+          <div class="font13 mt5" style="color:#f1f78f;" @click="showToast">厂家使用期至：{{endTime}}</div>
         </div>
       </div>
       <div class="font0" style="position:absolute;top:20px;right:14px;height:35px;">
@@ -198,6 +198,10 @@ export default {
     factoryInfo: {
       type: Object,
       default: {}
+    },
+    endTime: {
+      type: String,
+      default: ''
     },
     messages: {
       type: Number,
