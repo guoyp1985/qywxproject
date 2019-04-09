1cd1a135bd95b4eb83c1e2b4b9808916b4b578ba
guoyp1985 <605034716@qq.com>
2019-04-09 17:58:54 +0800

    gypadd

--- libs/Eleditor/index.js
+++ libs/Eleditor/index.js
@@ -189,8 +189,8 @@ var _buildEditorModule = function(_toolbars, _uid){
 						</ul>\
 					</div>\
 					<div class="Eleditor-textEditor-formats formats-area">\
-						<div class="Eleditor-textEditor-format"></div>\
-						<div class="Eleditor-textEditor-clean"></div>\
+						<div class="Eleditor-textEditor-format">格式刷</div>\
+						<div class="Eleditor-textEditor-clean">清空</div>\
 					</div>\
 					<div class="Eleditor-textEditor-formats btn-area">\
 						<div class="Eleditor-cancel"><text class="font12 color-gray">取消</text></div>\
