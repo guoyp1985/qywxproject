0958322e0dc595070572286333587121e7205c34
guoyp1985 <605034716@qq.com>
2019-04-22 15:51:10 +0800

    gypadd

--- src/pages/RetailerOrders.vue
+++ src/pages/RetailerOrders.vue
@@ -707,28 +707,28 @@ export default {
           if (this.selectedIndex === 0) {
             this.swiperChange()
           }
-          if (`${this.loginUser.retailerinfo.firstinfo.orderdeliver}` === '0' && this.query.from) {
-            this.$http.get(`${ENV.BokaApi}/api/retailer/info`).then(res => {
-              const data = res.data
-              if (data.flag) {
-                this.retailerInfo = data.data
-                this.loginUser.retailerinfo = this.retailerInfo
-                User.set(this.loginUser)
-                if (`${this.loginUser.retailerinfo.firstinfo.orderdeliver}` === '0' && this.query.from) {
-                  this.isFirst = true
-                  let finfo = FirstInfo.get()
-                  if (!finfo) {
-                    finfo = this.loginUser.retailerinfo.firstinfo
-                  }
-                  if (`${finfo.orderdeliver}` === '0') {
-                    this.showFirst = true
-                    finfo.orderdeliver = 1
-                  }
-                  FirstInfo.set(finfo)
-                }
-              }
-            })
-          }
+          // if (`${this.loginUser.retailerinfo.firstinfo.orderdeliver}` === '0' && this.query.from) {
+          //   this.$http.get(`${ENV.BokaApi}/api/retailer/info`).then(res => {
+          //     const data = res.data
+          //     if (data.flag) {
+          //       this.retailerInfo = data.data
+          //       this.loginUser.retailerinfo = this.retailerInfo
+          //       User.set(this.loginUser)
+          //       if (`${this.loginUser.retailerinfo.firstinfo.orderdeliver}` === '0' && this.query.from) {
+          //         this.isFirst = true
+          //         let finfo = FirstInfo.get()
+          //         if (!finfo) {
+          //           finfo = this.loginUser.retailerinfo.firstinfo
+          //         }
+          //         if (`${finfo.orderdeliver}` === '0') {
+          //           this.showFirst = true
+          //           finfo.orderdeliver = 1
+          //         }
+          //         FirstInfo.set(finfo)
+          //       }
+          //     }
+          //   })
+          // }
         }
       }
     }
