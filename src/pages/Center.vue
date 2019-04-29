ce174716f73a058c2d70ee46b3244b2592e13788
guoyp1985 <605034716@qq.com>
2019-04-29 19:47:19 +0800

    gypadd

--- src/pages/Center.vue
+++ src/pages/Center.vue
@@ -124,7 +124,7 @@ import TipLayer from '@/components/TipLayer'
 import ENV from 'env'
 import Time from '#/time'
 import Reg from '#/reg'
-import { Token, User, FirstInfo, SystemParams } from '#/storage'
+import { Token, User, FirstInfo } from '#/storage'
 let self = {}
 export default {
   components: {
