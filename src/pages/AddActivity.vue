487c9dec0d12ddc4129b96c85b0f8ac13b741980
guoyp1985 <605034716@qq.com>
2019-04-29 14:44:16 +0800

    gypadd

--- src/pages/AddActivity.vue
+++ src/pages/AddActivity.vue
@@ -302,10 +302,11 @@ export default {
     confirmpopup () {
       const self = this
       if (!this.selectpopupdata || !this.selectpopupdata.id) {
-        self.$vux.alert.show({
-          title: '',
-          content: '请选择商品'
-        })
+        self.$vux.toast.text('请选择商品', 'middle')
+        return false
+      }
+      if (this.selectpopupdata.fid > 0 && parseFloat(this.selectpopupdata.rebatein) <= 0) {
+        self.$vux.toast.text('佣金为0的厂家商品不能创建活动', 'middle')
         return false
       }
       if (this.selectpopupdata.allowcard) {
