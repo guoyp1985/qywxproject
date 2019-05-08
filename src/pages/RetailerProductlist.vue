2095f7037f5690e1c9f799389c7707ce94bea8a0
guoyp1985 <605034716@qq.com>
2019-05-08 13:12:13 +0800

    gypadd

--- src/pages/RetailerProductlist.vue
+++ src/pages/RetailerProductlist.vue
@@ -65,7 +65,6 @@
               <div v-else class="scroll_list ">
                 <router-link :to="{path:'/product',query:{id:item.id, wid: loginUser.uid}}" class="scroll_item mb10 font14 bg-white db list-shadow " v-for="(item,index) in tabData2" :key="index" style="color:inherit;">
                   <div v-if="item.fpid" class="ico1">货源</div>
-                  <div v-if="item.moderate == 0" class="ico down"></div>
               		<div class="t-table bg-white pt10 pb10">
               			<div class="t-cell pl12 v_middle" style="width:110px;">
                       <img class="imgcover v_middle" :src="getPhoto(item.photo)" style="width:100px;height:100px;" onerror="javascript:this.src='https://tossharingsales.boka.cn/images/nopic.jpg';"/>
