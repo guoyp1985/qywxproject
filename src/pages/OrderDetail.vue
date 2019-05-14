839c52fb795469bb0456ab7b87b776fd8a377c3f
guoyp1985 <605034716@qq.com>
2019-05-14 18:19:12 +0800

    gypadd

--- src/pages/OrderDetail.vue
+++ src/pages/OrderDetail.vue
@@ -139,7 +139,7 @@
         </x-dialog>
       </div>
       <div class="s-bottom flex_center pl12 pr12 list-shadow02 bg-white" style="height:50px;">
-        <router-link class="flex_cell flex_center color-white btn-bottom-red" to="/center">进入个人中心</router-link>
+        <div @click="toCenter" class="flex_cell flex_center color-white btn-bottom-red">进入个人中心</div>
       </div>
     </template>
     <div v-if="showRefundModal" class="auto-modal refund-modal flex_center">
@@ -218,6 +218,13 @@ export default {
     }
   },
   methods: {
+    toCenter () {
+      if (this.query.from) {
+        this.$wechat.miniProgram.navigateTo({url: `/pages/user`})
+      } else {
+        this.$router.push({path: '/center'})
+      }
+    },
     toProduct (item) {
       if (this.query.from) {
         this.$wechat.miniProgram.navigateTo({url: `${ENV.MiniRouter.product}?id=${item.pid}&wid=${item.wid}`})
