e3fdd45906cd48d8c8559ba661a6ca22a56328ad
1 <git@boka.cn>
2019-05-23 15:36:49 +0800

    ymladd

--- src/pages/MaterialBank.vue
+++ src/pages/MaterialBank.vue
@@ -21,16 +21,14 @@
                 <div class="picitem">
                   <div class="inner-sp flex_center" style="border:1px solid #e5e5e5;">
                     <div class="pofang flex_center" @click="clickPlay('productVideo', item)"><i class="al al-bofang font16"></i></div>
-                    <video
+                    <!-- <video
                       :src="item.video"
-                      webkit-playsinline=""
-                      playsinline="true"
                       x-webkit-airplay="true"
                       raw-controls=""
                       x5-video-player-type="h5"
                       x5-video-player-fullscreen="true"
                       x5-video-orientation="portrait">
-                    </video>
+                    </video> -->
                   </div>
                 </div>
               </template>
@@ -59,6 +57,8 @@
           :src="clickVideo"
           controls
           autoplay="true"
+          playsinline="true"
+          webkit-playsinline="true"
           x-webkit-airplay="true"
           raw-controls=""
           x5-video-player-type="h5"
@@ -267,7 +267,7 @@ export default {
   .play-icon{width:110px;height:110px;border:1px solid #e5e5e5;}
   // .timelinelist:last-child{margin-bottom:50px;}
   .pofang{
-    position:absolute;width:40px;height:40px;background-color:rgba(0,0,0,0.3);border-radius:50%;color:#fff;z-index:10;
+    width:40px;height:40px;background-color:rgba(0,0,0,0.3);border-radius:50%;color:#fff;z-index:10;
     .al{margin-left:5px;margin-top:3px;}
   }
   .inner-sp{width:90px;height:90px;background-color:#333;}
