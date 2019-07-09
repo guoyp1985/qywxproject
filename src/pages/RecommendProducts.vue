029e083b16315d76be773d76bf34c13bb8d4a0e4
guoyp1985 <605034716@qq.com>
2019-07-09 09:28:25 +0800

    gypadd

--- src/pages/RecommendProducts.vue
+++ src/pages/RecommendProducts.vue
@@ -50,7 +50,7 @@
           			<div class="picarea">
           				<div class="pic">
                     <img class="imgcover" :src="$util.getPhoto(item.photo)" onerror="javascript:this.src='https://tossharingsales.boka.cn/images/nopic.jpg';" />
-                    <div class="t-icon color-theme flex_center"><i class="al al-zhuanqian font18"></i><span>赚 {{$t('RMB')}}{{item.levelagent}}</span></div>
+                    <div class="t-icon color-theme flex_center"><i class="al al-zhuanqian font18"></i><span>赚 {{$t('RMB')}}{{item.salesrebate ? item.salesrebate : item.levelagent}}</span></div>
                   </div>
           			</div>
           			<div class="desbox" style="overflow:hidden;">
