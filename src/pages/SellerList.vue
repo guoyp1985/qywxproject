18c0238156bf5ade2251a6cf1225b3b922719025
yuanhuyh <873975766@qq.com>
2019-07-22 09:20:28 +0800

    yhadd

--- src/pages/SellerList.vue
+++ src/pages/SellerList.vue
@@ -183,15 +183,10 @@
                 </div>
               </template>
               <template v-else>
-                <div class="pro_box bg-page list_shadow pl12 pr12 pb15 border-box">
-                  <div class="pro_list_top"></div>
-                  <div class="rule pb12 pt12 pl12 pr12 border color-lightgray b_bottom_after list-shadow bg-white font12" style="margin-top: -4px;">
-                    <div>什么是待审核卖家？</div>
-                    <div>指拥有自己的店铺，利用自己的客户群体兼职销售本厂家商品的卖家。</div>
-                    <div>如何发展兼职卖家？</div>
-                    <div>1、通过厂家介绍界面申请加盟的卖家即可成为兼职卖家。</div>
-                    <div>2、卖家通过渠道列表选择并加盟厂家商品时，即可成为兼职卖家。</div>
-                  </div>
+                <div class="flex_around">
+                  <!-- <div class="doBtn" @click="allchoose()">全选</div> -->
+                  <div class="doBtn" @click="agree()">同意</div>
+                  <div class="doBtn" @click="refuse()">拒绝</div>
                 </div>
                 <div class="scroll_list ">
                   <div class="scroll_item mb10 font14 bg-white db list-shadow " v-for="(item,index) in tabData5" :key="item.id" style="color:inherit;">
@@ -207,9 +202,7 @@
                         <div class="clamp1 font12 color-orange">销售额: {{ $t('RMB') }}{{item.salesmoney}}</div>
                       </div>
                       <div class="align_right t-cell v_middle w80">
-                        <div class="btnicon bg-red color-white font12" @click="controlPopup1(item,index)">
-                          <i class="al al-asmkticon0165 v_middle"></i>
-                        </div>
+                        <input type="checkbox" v-model="idArr" :value="item.uid" :checked="checked" :data-uid="item.uid" :data-fid="item.fid"/>
                       </div>
                 		</div>
                   </div>
@@ -395,7 +388,9 @@ export default {
       showUploaderPopup: false,
       selectUploader: null,
       userData: [],
-      disUserData: false
+      disUserData: false,
+      checked: false,
+      idArr: []
     }
   },
   methods: {
@@ -637,6 +632,21 @@ export default {
         }
       }
     },
+    allchoose () {
+      if (this.checked) {
+        this.checked = false
+      } else {
+        this.checked = true
+      }
+    },
+    agree () {
+      console.log('------ids--')
+      console.log(this.idArr)
+    },
+    // getValue (event) {
+    //   console.log('----------')
+    //   console.log(event.currentTarget.dataset.uid)
+    // },
     submitLevel () {
       const self = this
       if (!self.selectLevel) {
@@ -891,5 +901,7 @@ export default {
     background-position: center;
     background-size: 100%;
   }
+  .doBtn{height: 44px;line-height: 44px;width: 33.3%;text-align: center;}
+  .flex_around{display: flex;justify-content: space-around; align-items: center;}
 }
 </style>
