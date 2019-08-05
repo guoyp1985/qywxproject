fdf1672896b6dfac3302f01766dcadce498b3827
guoyp1985 <605034716@qq.com>
2019-08-05 16:50:54 +0800

    gypadd

--- src/pages/AddOrder.vue
+++ src/pages/AddOrder.vue
@@ -135,7 +135,7 @@
         <div class="t-table h_100 align_center">
   				<div class="t-cell h_100 v_middle">需付：<span class="color-orange1">{{ $t('RMB') }}{{ payPrice }}</span></div>
   				<div class="t-cell h_100 v_middle w100 bg-orange1 color-white" v-if="allowSend" @click="submitOrder">确认订单</div>
-  				<div class="t-cell h_100 v_middle w100 bg-gray color-white" v-else>确认订单</div>
+  				<div class="t-cell h_100 v_middle bg-gray color-white" style="width:130px;" v-else>该地区无法派送</div>
   			</div>
       </div>
       <div v-transfer-dom class="x-popup">
