498c11ef5c0f09d430f757016f2cf5024f6477f3
guoyp1985 <605034716@qq.com>
2019-08-08 18:17:58 +0800

    gypadd

--- src/main.js
+++ src/main.js
@@ -139,6 +139,7 @@ const matchExclude = url => {
 // let responseFail = false
 // 请求拦截器
 Vue.http.interceptors.request.use(config => {
+  alert('进入到了请求拦截器')
   if (!matchExclude(config.url)) {
     config.cancelToken = new CancelToken(c => {
       pendings.push({ u: config.url + '&' + config.method, f: c })
@@ -148,7 +149,6 @@ Vue.http.interceptors.request.use(config => {
     console.log(`interceptors: ${config.url}`)
     console.log(`interceptors: ${JSON.stringify(token)}`)
     if (Token.isExpired()) {
-      alert('token过期了')
       // console.log(config.url)
       cancelAllPendings(config)
       access((path) => {
