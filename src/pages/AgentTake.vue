6e35d2c29c9d3ed33e9d7df6bee9c17745bd912a
guoyp1985 <605034716@qq.com>
2019-08-12 13:47:10 +0800

    gypadd

--- src/pages/AgentTake.vue
+++ src/pages/AgentTake.vue
@@ -366,7 +366,7 @@ export default {
         this.$vux.toast.text('请输入提货数量', 'middle')
         return false
       }
-      if (isNaN(this.takeQuantity) || parseFloat(this.takeQuantity) < 0) {
+      if (isNaN(this.takeQuantity) || parseFloat(this.takeQuantity) <= 0 || this.takeQuantity.indexOf('.') > -1 || this.takeQuantity.indexOf(',') > -1) {
         this.$vux.toast.text('请输入正确的提货数量', 'middle')
         return false
       }
