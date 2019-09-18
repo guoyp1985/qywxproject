ae62c87f1d149e0dc8aa99e7a426dfdb8b8b6dcc
guoyp1985 <605034716@qq.com>
2019-09-18 16:57:44 +0800

    gypadd

---
+++ src/pages/RedirectFactory.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import ENV from 'env'
+import { User } from '#/storage'
+import urlParse from 'url-parse'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    refresh () {
+      const self = this
+      self.$vux.loading.show()
+      const user = User.get()
+      const query = this.$route.query
+      if (user.fid) {
+        this.$router.push({path: '/factory', query: {fid: user.fid}})
+      } else {
+        this.$router.push({path: '/addFactory', query: {share_uid: query.share_uid}})
+      }
+    }
+  },
+  activated () {
+    this.refresh()
+  }
+}
+</script>
