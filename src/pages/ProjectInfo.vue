e1877a47c9c0b4f93a0295653c139c145cadc44b
guoyp1985 <605034716@qq.com>
2019-09-18 19:20:46 +0800

    gypadd

--- src/pages/ProjectInfo.vue
+++ src/pages/ProjectInfo.vue
@@ -15,8 +15,8 @@
         <img class="avatarimg3 imgcover v_middle" src="" onerror="javascript:this.src='https://tossharingsales.boka.cn/images/user.jpg';" />
       </div>
       <div class="t-cell v_middle">
-        <div class="clamp1 font14">完美生活旗舰店</div>
-        <div v-if="selectedIndex === 0" class="clamp1 mt5 font14" style="color:#ea3a3a">项目收入: ￥0.00</div>
+        <div class="clamp1 font14">{{infoData.factoryinfo.title}}</div>
+        <div v-if="selectedIndex === 0" class="clamp1 mt5 font14" style="color:#ea3a3a">项目收入: ￥{{infoData.mymonthmoney}}</div>
         <div v-else class="clamp1 mt5 font14" style="color:#ea3a3a">合作模式: 每月分佣</div>
       </div>
       <!-- <div class="t-cell align_right v_middle">
