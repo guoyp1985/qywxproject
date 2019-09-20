d8448d04144826667d8d3437f4c0bf117b747fc7
guoyp1985 <605034716@qq.com>
2019-09-20 13:27:31 +0800

    gypadd

--- src/pages/FactoryProductlist.vue
+++ src/pages/FactoryProductlist.vue
@@ -36,7 +36,7 @@
                             <span class="v_middle db-in">库存: {{ item.storage }}{{item.unit}}</span>
                             <span class="v_middle db-in ml5">已售: {{ item.truesaled }}{{item.unit}}</span>
                         </div>
-                        <div class="font12">利润空间:<span class="color-red"> {{ $t('RMB') }}{{ item.lirun }}</span></div>
+                        <div class="font12" v-if="item.fromfid">利润空间:<span class="color-red"> {{ $t('RMB') }}{{ item.lirun }}</span></div>
                         <!-- <div class="clamp1 mt5">
                             <span class="v_middle db-in">销售佣金: {{ item.salesrebate }}</span>
                             <span class="v_middle db-in ml5">推荐佣金: {{ item.superrebate }}</span>
