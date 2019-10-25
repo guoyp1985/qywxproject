fe176ef79f97f277268e20bcdb177d7aa1e7c6f5
guoyp1985 <605034716@qq.com>
2019-10-25 11:53:24 +0800

    gypadd

--- src/pages/SellerList.vue
+++ src/pages/SellerList.vue
@@ -1306,7 +1306,7 @@ export default {
             break
           }
         }
-        if (!(self.loginUser.fid && parseInt(self.loginUser.fid) === parseInt(self.$route.query.id)) && !isAdmin && !query.fromapp === 'factory') {
+        if (!(self.loginUser.fid && parseInt(self.loginUser.fid) === parseInt(self.$route.query.id)) && !isAdmin && !self.query.fromapp === 'factory') {
           this.$vux.loading.hide()
           self.showSos = true
           self.showContainer = false
