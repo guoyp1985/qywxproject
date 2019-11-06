376b0ccb69667844bd178394718e8cd37bae8eae
yuanhuyh <873975766@qq.com>
2019-11-06 17:27:30 +0800

    yhadd

--- src/pages/RetailerOrders.vue
+++ src/pages/RetailerOrders.vue
@@ -77,7 +77,8 @@
                     <div class="qbtn4 font12" style="padding:1px 14px;" @click="uploaddeliver(item,index1)">{{ $t('Deliver goods') }}</div>
                   </div>
                   <div class="t-cell v_middle appendcontrol align_right w80" v-if="item.flag == 3">
-                    <router-link :to="{path: '/deliverinfo', query: {id: item.id}}" class="qbtn4 color-orange7 font12" style="border:1px solid #ee9f25;padding:1px 8px">{{ $t('View deliver') }}</router-link>
+                    <router-link v-if="query.fromapp != 'factory'" :to="{path: '/deliverinfo', query: {id: item.id}}" class="qbtn4 color-orange7 font12" style="border:1px solid #ee9f25;padding:1px 8px">{{ $t('View deliver') }}</router-link>
+                    <router-link v-if="query.fromapp == 'factory'" :to="{path: '/deliverinfo', query: {id: item.id, fromapp: 'factory'}}" class="qbtn4 color-orange7 font12" style="border:1px solid #ee9f25;padding:1px 8px">{{ $t('View deliver') }}</router-link>
                   </div>
                 </div>
               </div>
