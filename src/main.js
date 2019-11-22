a5365b9182ef4ed7e89b3742bab3118e8c204e7b
simon <siemon83@gmail.com>
2019-11-22 21:49:27 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -374,7 +374,7 @@ if (!Token.get() || Token.isExpired()) {
     console.log(`Entry: ${path}`)
     router.replace({path: path})
     for (let i = 0; i < bugList.length; i++) {
-      console.log(User.get())
+      console.log(bugList[i])
       if (bugList[i].uid === User.get().uid) {
         alert('准备渲染页面')
       }
