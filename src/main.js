9ac6d43bb691c026e38d78833783aef0eb26956f
simon <siemon83@gmail.com>
2019-11-23 12:21:13 +0800

    upload by simon

--- src/main.js
+++ src/main.js
@@ -411,20 +411,16 @@ try {
       router.replace({path: path})
       for (let i = 0; i < ENV.DebugList.length; i++) {
         if (ENV.DebugList[i].uid === User.get().uid) {
-          alertStack.push(
-            () => {
-              vue.$vux.alert.show({
-                title: '提示',
-                content: `token:${Token.get().token} :: 开始渲染页面`,
-                onShow () {
-                  console.log('Plugin: I\'m showing')
-                },
-                onHide () {
-                  console.log('Plugin: I\'m hiding')
-                }
-              })
+          vue.$vux.alert.show({
+            title: '提示',
+            content: `token:${Token.get().token} :: 开始渲染页面`,
+            onShow () {
+              console.log('Plugin: I\'m showing')
+            },
+            onHide () {
+              console.log('Plugin: I\'m hiding')
             }
-          )
+          })
         }
       }
       render()
