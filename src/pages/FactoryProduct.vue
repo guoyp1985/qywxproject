1521262709b74a62399fc308e7a6c3b27f8757e4
yuanhuyh <873975766@qq.com>
2020-02-26 14:27:46 +0800

    yhadd

--- src/pages/FactoryProduct.vue
+++ src/pages/FactoryProduct.vue
@@ -88,7 +88,7 @@
               <span class="bold">推荐人佣金:</span>
               <span class="bold">{{ $t('RMB') }}</span>
               <span class="bold" v-if="productdata.minsuperrebate && productdata.maxsuperrebate && productdata.minsuperrebate != productdata.maxsuperrebate">{{ productdata.minsuperrebate }}-{{productdata.maxsuperrebate}}</span>
-              <span class="bold" v-else>{{ productdata.superrebate }}</span>
+              <span class="bold" v-else>{{ productdata.minsuperrebate }}</span>
               <i class="al al-bangzhu font16 color-theme ml10" @click="clickHelp"></i>
             </div>
           </div>
@@ -99,7 +99,7 @@
               <span class="bold">销售佣金:</span>
               <span class="bold"> {{ $t('RMB') }}</span>
               <span class="bold" v-if="productdata.minsalesrebate && productdata.maxsalesrebate && productdata.minsalesrebate != productdata.maxsalesrebate">{{ productdata.minsalesrebate }}-{{productdata.maxsalesrebate}}</span>
-              <span class="bold" v-else>{{ productdata.salesrebate }}</span>
+              <span class="bold" v-else>{{ productdata.minsalesrebate }}</span>
               <i class="al al-bangzhu font16 color-theme ml10" @click="clickHelp1"></i>
             </div>
           </div>
