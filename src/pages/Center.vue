d89b3c461fc01f86e0b1c37e2d978cd6aa2daf20
simon <siemon83@gmail.com>
2020-07-22 10:37:15 +0800

    create by simon

--- src/pages/Center.vue
+++ src/pages/Center.vue
@@ -141,6 +141,18 @@
           </script>
         </wx-open-launch-weapp>
       </template> -->
+      <template>
+        <wx-open-launch-weapp
+          id="launch-btn3"
+          username="gh_dc6e3c73bc4c"
+          @launch="handleLaunchFn"
+          @error="handleErrorFn">
+          <script type="text/wxtag-template">
+            <style>.btn { display: flex;align-items: center; }</style>
+            <button class="wx-btn">跳转小程序</button>
+          </script>
+        </wx-open-launch-weapp>
+      </template>
     </div>
     <template v-if="showTip">
       <tip-layer buttonTxt="点击此处联系管理员" content="请联系管理员续费后，再来使用厂家功能哦！" @clickClose="closeTip" @clickButton="toApply"></tip-layer>
@@ -159,8 +171,6 @@ import ENV from 'env'
 import Time from '#/time'
 import Reg from '#/reg'
 import { Token, User, FirstInfo } from '#/storage'
-const jweixin = require('../../static/jweixin')
-console.log(jweixin.config)
 let self = {}
 export default {
   components: {
