141521ff6404eff221bac09315306d5774ffb2f4
guoyp1985 <605034716@qq.com>
2020-07-24 14:30:23 +0800

    gypadd

--- src/pages/AddFpimportProduct.vue
+++ src/pages/AddFpimportProduct.vue
@@ -249,14 +249,6 @@
               <div class="t-cell v_middle align_right font12" style="width:20px;">元</div>
             </div>
           </div>
-          <div class="form-item required bg-white">
-            <div class="t-table">
-              <div class="t-cell title-cell w80 font14 v_middle">供应商</div>
-              <div class="t-cell input-cell v_middle" style="position:relative;">
-                <x-input v-model="submitdata.manufacturer" type="text" class="input priceInput" name="manufacturer" placeholder="供应商" ></x-input>
-              </div>
-            </div>
-          </div>
           <div class="pt10 bg-page"></div>
           <div class="form-item required bg-white" v-if="!optionsData.length">
             <div class="flex_row">
