608a474bfc3abd3fd1559775d52c7740b671186d
guoyp1985 <605034716@qq.com>
2020-09-25 11:51:29 +0800

    gypadd

--- src/pages/RecommendProducts.vue
+++ src/pages/RecommendProducts.vue
@@ -351,7 +351,7 @@ export default {
       if (this.query.id) {
         params.fid = this.query.id
       }
-      this.$router.push({path: '/factoryProduct', query: params})
+      this.$router.push({path: '/fpimportProduct', query: params})
     },
     handleScroll (refname) {
       let swiperOuter = self.$refs['swiperOuter']
@@ -409,12 +409,11 @@ export default {
         params.ascdesc = this.pricecss
       }
       if (this.selectedIndex === 0) {
-        params.recommend = 2
+        // params.recommend = 2
       } else if (this.selectedIndex === 1) {
-        params.from = 'origin'
+        // params.from = 'origin'
       } else {
-        // params.orderby = 'saled'
-        params.from = 'origin'
+        // params.from = 'origin'
         params.classid = self.classData[self.selectedIndex].id
       }
       if (this.searchword !== '') {
@@ -423,8 +422,12 @@ export default {
       params.fid = ENV.SourceFid
       params.from = 'factory'
       params.moderate = 1
+      console.log('进入到了请求商品列表')
+      console.log(params)
       // self.$http.post(`${ENV.BokaApi}/api/list/factoryproduct`, params).then((res) => {
-      self.$http.get(`${ENV.BokaApi}/api/factory/getfpimportList`, params).then((res) => {
+      self.$http.get(`${ENV.BokaApi}/api/factory/getfpimportList`, {
+        params: params
+      }).then((res) => {
         self.$vux.loading.hide()
         const data = res.data
         const retdata = data.data ? data.data : data
