89edac8d0ef9765ad5975acd805a6677bf8ba3da
guoyp1985 <605034716@qq.com>
2021-01-23 21:38:43 +0800

    gypadd

--- src/pages/Jumpmp.vue
+++ src/pages/Jumpmp.vue
@@ -24,12 +24,13 @@
   <div class="w_100 h_100 bg-white flex_center jumpmp-page">
     <div class="public-web-container">
       <div>正在打开 【灰太狼】小程序</div>
-      <div v-if="disLoading" class="btn" @click="jumpEvent">打开小程序</div>
+      <div v-if="disLoading" class="btn" @click="openWeapp">打开小程序</div>
     </div>
   </div>
 </template>
 
 <script>
+const jweixin = require('../../static/jweixin')
 const cloud = require('../../static/cloud')
 export default {
   data () {
@@ -52,7 +53,7 @@ export default {
       }
       location.href = res.result.openlink
     },
-    async jumpEvent () {
+    async refresh () {
       let c = new cloud.Cloud({
         identityless: true, // 必填，表示是未登录模式
         resourceAppid: 'wx1b22fe75584085ee', // 资源方 AppID
@@ -68,8 +69,6 @@ export default {
         this.disLoading = false
         throw e
       }
-    },
-    refresh () {
     }
   },
   activated () {
