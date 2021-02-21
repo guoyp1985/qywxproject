de4dec1c6461dbd2f4107e647631c3100ac5384a
guoyp1985 <605034716@qq.com>
2021-02-21 17:50:49 +0800

    gypadd

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -1,50 +1,139 @@
 <style lang="less">
 .activity-page{
-  .box-outer{padding:20px;box-sizing:border-box;}
-  .txt1{text-align:center;font-size:18px;font-weight:bold;}
-  .txt2{text-align:center;margin-top:5px;}
-  .btn{width:80%;max-width:300px;height:40px;line-height:40px;text-align:center;border-radius:60px;background-color:#07c160;color:#fff;margin:20px auto 10px;}
+  height:100%;background-color:#f94929;
+  .bg-image1{width:100%;}
+  .row1{
+    width:100%;
+    img{width:100%;display:block;}
+  }
+  .row2{
+    width:100%;position:relative;
+    img{width:100%;display:block;}
+    .row2-inner{position:absolute;left:0;top:0;right:0;bottom:0;}
+  }
+  .pic-area{
+    width:53%;
+    .pic{
+      width:100%;position:relative;
+      img{width:100%;}
+      .txt1{
+        width:34%;max-height:100%;position:absolute;left:33%;top:34%;
+        .inner{width:100%;max-height:100%;position:relative;}
+        .inner:after{padding-top:100%;content:"";display:block;}
+        .btn{position:absolute;left:0;top:0;right:0;bottom:0;font-size:25px;color:#f95a51;}
+        .btn.disable{color:#999;}
+      }
+      .txt2{position:absolute;left:0;bottom:15%;right:0;text-align:center;color:#F9EA72;font-size:15px;letter-spacing:1px;}
+    }
+  }
+}
+.result-modal{
+  .inner{
+    width:90%;position:relative;
+    .pic-outer{
+      width:100%;padding-bottom:107%;position:relative;
+      .pic{
+        position:absolute;left:0;top:0;right:0;bottom:0;
+        img{width:100%;height:100%;}
+      }
+    }
+  }
+  .top-txt{
+    position:absolute;left:0;top:22%;right:0;width:100%;height:12%;color:#7a2716;font-weight:bold;font-size:30rpx;
+    .big{font-size:40rpx;color:#ff6a61;}
+  }
+  .con-txt{
+    position:absolute;left:0;top:33%;right:0;width:100%;height:29%;color:#7a2716;font-weight:bold;font-size:30rpx;
+    .big{font-size:80rpx;color:#ff6a61;}
+  }
+  .txt-list{
+    position:absolute;left:0;bottom:10%;right:0;display:flex;
+    .item{flex:1;color:#f8edad;box-sizing: border-box;}
+    .item:nth-child(1){padding-right:20rpx;}
+    .item:nth-child(2){padding-left:20rpx;}
+  }
+  .close-area{position:absolute;left:0;bottom:-120rpx;right:0;height:100rpx;}
+  .close-area .al{font-weight:bold;font-size:70rpx;color:#fff;}
 }
 </style>
 <template>
   <div class="activity-page">
     <template v-if="viewData && viewData.id">
       <template v-if="viewData.type == 'cardcommon'">
-        <div class="box-outer">
-          <div class="txt1">{{facemoney}}元优惠券</div>
-          <div class="btn">立即领取</div>
-          <div class="b_top_after flex_left pt10 pb10 color-gray">
-            <div>有效期</div>
-            <div class="flex_cell flex_right">{{viewData.starttime_str}}-{{viewData.endtime_str}}</div>
-          </div>
-          <div class="b_top_after flex_left pt10 pb10 color-gray">
-            <div>使用门槛</div>
-            <div class="flex_cell flex_right">满{{ordermoney}}元可用</div>
+        <div class="row1">
+          <img src="https://tossharingsales.boka.cn/minigxk/luck/bg3.png" />
+        </div>
+        <div class="row2">
+          <img src="https://tossharingsales.boka.cn/minigxk/luck/bg2.png" />
+          <div class="row2-inner">
+            <div class="w_100 flex_center">
+              <div class="pic-area">
+                <div class="pic">
+                  <img mode="widthFix" src="https://tossharingsales.boka.cn/minigxk/luck/hb1.png" />
+                  <div class="txt1">
+                    <div class="inner">
+                      <div class="btn flex_center" @click="openEvent">開</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </template>
     </template>
+    <div v-transfer-dom class="x-popup">
+      <popup v-model="showResultModal" height="100%" class="result-modal">
+        <div class="popup1 h_100 flex_center">
+          <div class="inner">
+            <div class="pic-outer">
+              <div class="pic">
+                <img src="https://tossharingsales.boka.cn/minigxk/luck/hb2.png" />
+              </div>
+            </div>
+            <div class="top-txt flex_center">恭喜你获得<span class="big">优惠券</span></div>
+            <div class="con-txt flex_center">
+              <div class="w_100 align_center">
+                <div class="big">￥{{facemoney}}</div>
+                <div>满{{ordermoney}}可用</div>
+              </div>
+            </div>
+            <div class="close-area flex_center">
+              <div class="al al-close" @click="closeResultModal"></div>
+            </div>
+          </div>
+        </div>
+      </popup>
+    </div>
   </div>
 </template>
 
 <script>
-import { Tab, TabItem } from 'vux'
+import { Tab, TabItem, TransferDom, Popup } from 'vux'
 import ENV from 'env'
 import Time from '../../libs/time'
 import { User } from '#/storage'
 
 export default {
-  components: { Tab, TabItem },
+  directives: { TransferDom },
+  components: { Tab, TabItem, Popup },
   data () {
     return {
       query: {},
       loginUser: {},
       viewData: {},
       ordermoney: '0.00',
-      facemoney: '0.00'
+      facemoney: '0.00',
+      showResultModal: false
     }
   },
   methods: {
+    openEvent () {
+      this.showResultModal = true
+    },
+    closeResultModal () {
+      this.showResultModal = false
+    },
     getData () {
       const infoparams = {id: this.query.id, module: 'miniactivity'}
       if (this.query['share_uid']) {
